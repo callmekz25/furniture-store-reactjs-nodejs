1ebb70761abd646fa48bb997fcb37b688ff22f81
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-04 00:37:53 +0700

    Add router to get products in collections

--- backend/routes/public/products.js
+++ backend/routes/public/products.js
@@ -4,13 +4,15 @@ import {
   deleteProduct,
   getProductBySlug,
   getProducts,
+  getProductsByCollectionOrCategory,
 } from "../../controllers/productController.js";
 import authMiddleware from "../../middleware/authMiddleware.js";
 import upload from "../../middleware/multerMiddleware.js";
 const router = express.Router();
 
-router.get("/products", getProducts);
-router.get("/product/:slug", authMiddleware, getProductBySlug);
+router.get("/collections", getProducts);
+router.get("/collections/:slug", getProductsByCollectionOrCategory);
+router.get("/product/:slug", getProductBySlug);
 router.post("/product", upload.array("files", 10), addProduct);
 router.delete("/product", deleteProduct);
 export default router;
