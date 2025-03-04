ffad8aff1f4e1407ddb69ee65a0c4e9047dc1635
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-04 13:10:51 +0700

    Add query params to get products by filter

--- frontend/src/hooks/useProductsByCollectionOrCategory.ts
+++ frontend/src/hooks/useProductsByCollectionOrCategory.ts
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductsByCollectionOrCategory } from "@/api/product";
-const useProductsByCollectionOrCategory = (slug: string) => {
+
+const useProductsByCollectionOrCategory = (
+  slug: string,
+  searchParams: URLSearchParams
+) => {
   return useQuery({
-    queryKey: ["products", slug],
-    queryFn: () => getProductsByCollectionOrCategory(slug),
+    queryKey: ["products", slug, searchParams.toString()],
+    queryFn: () => getProductsByCollectionOrCategory(slug, searchParams),
     staleTime: 1000 * 60 * 30,
   });
 };
