80bb365fe264bcdf8b3ac4d02e02abad93cecbe2
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-04 13:09:15 +0700

    Update

--- frontend/src/components/CategorySection.tsx
+++ frontend/src/components/CategorySection.tsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import CATEGORIES from "../constants/categories";
 import { useRef } from "react";
+import { Link } from "react-router-dom";
 const CategorySection = () => {
   let sliderRef = useRef(null);
 
@@ -54,18 +55,31 @@ const CategorySection = () => {
             className="relative  rounded-md group overflow-hidden w-full px-3"
           >
             {/* Ảnh (chỉ zoom khi hover) */}
-            <div className=" hover:cursor-pointer w-full overflow-hidden rounded-md">
+            <Link
+              to={`/collections/${category.slug}`}
+              className=" hover:cursor-pointer block w-full overflow-hidden rounded-md"
+            >
               <img
                 src={category.image}
                 alt={category.label}
-                className="w-full  h-full object-cover transition-transform duration-300 group-hover:scale-110"
+                className="w-full  h-full object-cover  transition-transform duration-300 group-hover:scale-110"
               />
-            </div>
+            </Link>
 
             {/* Nội dung (không bị ảnh hưởng khi hover) */}
             <div className="absolute bottom-0 left-0 w-full  text-center flex flex-col overflow-hidden items-center justify-center py-3  transition-all duration-300 ">
-              <a className="text-lg font-bold text-red-700">{category.label}</a>
-              <span className="text-sm text-gray-700">Xem ngay</span>
+              <Link
+                to={`/collections/${category.slug}`}
+                className="text-lg font-bold text-red-700"
+              >
+                {category.label}
+              </Link>
+              <Link
+                to={`/collections/${category.slug}`}
+                className="text-sm text-gray-700"
+              >
+                Xem ngay
+              </Link>
             </div>
           </div>
         ))}
