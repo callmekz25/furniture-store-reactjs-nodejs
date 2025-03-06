05ad5d74bc0bb4a7ce4aa99e15f53ab9f7b89382
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-06 23:58:41 +0700

    Add constants sorts to sort query parameters

---
+++ frontend/src/constants/sorts.ts
@@ -0,0 +1,27 @@
+const SORTS = [
+  {
+    label: "Giá: Tăng dần",
+    type: "price.asc",
+  },
+  {
+    label: "Giá: Giảm dần",
+    type: "price.desc",
+  },
+  {
+    label: "Tên: A-Z",
+    type: "title.asc",
+  },
+  {
+    label: "Tên: Z-A",
+    type: "title.desc",
+  },
+  {
+    label: "Cũ nhất",
+    type: "createdAt.asc",
+  },
+  {
+    label: "Mới nhất",
+    type: "createdAt.desc",
+  },
+];
+export default SORTS;
