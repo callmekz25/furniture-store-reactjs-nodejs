94cb8afa407a2cb3a3eea7156256c4cd93670485
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-09 00:16:05 +0700

    Update token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,14 +3,22 @@ import { JWT_SECRET } from "../constants.js";
 const generateAccessToken = (user) => {
   // Lần đầu đăng nhập sẽ user._id các lần sau truy cập token sẽ là userId
   const userId = user._id ? user._id.toString() : user.userId;
-  return jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "15m",
-  });
+  return jwt.sign(
+    { userId: userId, name: user.name, email: user.email },
+    JWT_SECRET,
+    {
+      expiresIn: "15m",
+    }
+  );
 };
 const generateRefreshToken = (user) => {
   const userId = user._id ? user._id.toString() : user.userId;
-  return jwt.sign({ userId }, JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  return jwt.sign(
+    { userId: userId, name: user.name, email: user.email },
+    JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
 };
 export { generateAccessToken, generateRefreshToken };
