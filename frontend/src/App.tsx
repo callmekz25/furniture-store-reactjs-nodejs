53b788f32496abc178e1dbc1f24c5d5e7400f4e2
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-09 00:16:27 +0700

    Get user info when app run

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import "./App.css";
-import UserRoutes from "./routes/userRoutes";
-import AdminRoutes from "./routes/adminRoutes";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import { useEffect } from "react";
 
+import { getUserThunk } from "./redux/actions/auth.action";
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<UserRoutes />} />
-        <Route path="/admin/*" element={<AdminRoutes />} />
-      </Routes>
-    </Router>
-  );
+  const dispatch = useAppDispatch();
+  const { loading } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getUserThunk());
+  }, [dispatch]);
+  if (loading) {
+    return <p>Loading User....</p>;
+  }
+  return <RouterProvider router={router} />;
 };
 
 export default App;
