e76eff9f1bfd87607b36bfe07ae2f24a0c20ad28
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-10 01:18:33 +0700

    Add overview for admin

---
+++ frontend/src/components/admin/overviewCard.tsx
@@ -0,0 +1,21 @@
+const OverviewCard = ({
+  title,
+  descr,
+  total,
+}: {
+  title: string;
+  descr: string;
+  total: number;
+}) => {
+  return (
+    <div className="flex-1 rounded-xl border border-gray-300 p-5 flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
+        <h3 className=" text-lg font-bold">{title}</h3>
+        <p className="text-sm font-medium text-gray-500">{descr}</p>
+      </div>
+      <h4 className="text-3xl font-bold mt-2">{total}</h4>
+    </div>
+  );
+};
+
+export default OverviewCard;
