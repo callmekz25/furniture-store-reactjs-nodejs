1af59b5024e0c3dd2bcbdedb6559c75739c02d4e
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-10 01:23:44 +0700

    Handle query infinite

--- frontend/src/pages/user/collection.tsx
+++ frontend/src/pages/user/collection.tsx
@@ -1,44 +1,71 @@
 import useProductsByCollectionOrCategory from "@/hooks/useProductsByCollectionOrCategory";
 import Layout from "@/layouts/userLayout";
 import { useParams, useSearchParams } from "react-router-dom";
-import { FunnelIcon } from "@heroicons/react/24/outline";
+import { ChevronRightIcon, FunnelIcon } from "@heroicons/react/24/outline";
 import CardProduct from "@/components/user/cardProduct";
 import SideBarFilter from "@/components/user/sideBarFilter";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useAppDispatch } from "@/redux/hook";
 import { openFilterMenu } from "@/redux/slices/filter-menu.slice";
 import { useAppSelector } from "@/redux/hook";
-import Newsletter from "@/components/user/newsLetter";
+
 import useHiddenScroll from "@/hooks/useHiddenSscroll";
 import IProduct from "@/interfaces/product.interface";
-const Collection = () => {
-  const [typeCollection, setTypeCollection] = useState<string>();
 
+const Collection = () => {
+  // Dùng ref để giá trị không bị re-render chỉ mount 1 lần vì cố định
+  const suppliersRef = useRef<string[] | null>(null);
+  const typeRef = useRef<Record<string, any> | null>(null);
   const { slug } = useParams<string>();
-  const [searchParams] = useSearchParams();
-  const { data, isLoading, error } = useProductsByCollectionOrCategory(
-    slug,
-    searchParams
-  );
+
   const dispatch = useAppDispatch();
   const { isOpenMenuFilter } = useAppSelector((state) => state.filterMenu);
-  const [suppliers, setSuppliers] = useState<[string] | []>([]);
-  // Dùng state vì suppliers và collection name nằm trong response của api nên nếu search query params thay đổi thì response thay đổi làm re-render props của sidebar không cần thiết
-  useEffect(() => {
-    if (data?.suppliers) {
-      setSuppliers(data.suppliers);
+
+  const [quantityProducts, setQuantityProducts] = useState<number | null>();
+
+  const [searchParams] = useSearchParams();
+  const { data, isLoading, error, isFetching, fetchNextPage, hasNextPage } =
+    useProductsByCollectionOrCategory(slug, searchParams);
+  // Data của useInfiniteQuery là 1 object chứa pageParams: array là các số trang đã query rồi và pages: array chứa dữ liệu trả về từ api
+  // Hoạt động bằng cách nối mảng nhưng chỉ nối products tránh trùng lặp key của suppliers và type
+  // Do pages trả về 1 array chứa các response phải làm phẳng mảng
+  const mergedData = useMemo(() => {
+    if (data) {
+      const products = data.pages.flatMap((page) => page.products);
+
+      // Chỉ gán suppliers và type nếu chưa có giá trị
+      if (!suppliersRef.current && data.pages[0]?.suppliers) {
+        suppliersRef.current = data.pages[0].suppliers;
+      }
+      if (!typeRef.current && data.pages[0]?.type) {
+        typeRef.current = data.pages[0].type;
+      }
+      return {
+        products,
+        suppliers: suppliersRef.current || [],
+        type: typeRef.current || {},
+      };
     }
-  }, [data?.suppliers]);
+
+    return { products: [], suppliers: [], type: {} };
+  }, [data]);
+
   useEffect(() => {
-    if (data?.type.name) {
-      setTypeCollection(data.type.name);
+    if (mergedData.products.length) {
+      setQuantityProducts(mergedData.products.length);
     }
-  }, [data?.type.name]);
+  }, [mergedData.products.length]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [data?.products]);
+  }, [mergedData.products]);
+
   useHiddenScroll(isOpenMenuFilter);
+
+  if (error) {
+    return <p>Lỗi xảy ra...</p>;
+  }
+
   return (
     <Layout>
       <section className="break-point flex flex-wrap min-h-screen pb-8">
@@ -48,28 +75,24 @@ const Collection = () => {
         ></div>
         {/* Phần filter */}
         <div className="lg:w-[25%] min-h-full lg:flex-[0_0_25%]   lg:max-w-[25%] w-[85%] flex-[0_0_85%] max-w-[85%] ">
-          <SideBarFilter suppliers={suppliers} />
+          <SideBarFilter suppliers={suppliersRef.current} />
         </div>
 
         <div className="lg:flex-[0_0_75%] lg:max-w-[75%] flex-[0_0_100%]  max-w-full lg:py-10 py-3 lg:px-4 px-1">
           <div className="lg:flex lg:items-center  lg:justify-between lg:px-0 px-3">
             <div className="flex items-center gap-8">
               <h1 className="lg:text-[24px] text-[22px] font-bold text-red-700">
-                {typeCollection}
+                {typeRef?.current?.name}
               </h1>
               <span className="text-sm font-normal  items-center gap-2 lg:flex hidden">
-                <span className="font-bold">
-                  {isLoading ? "Loading..." : data.products.length}
-                </span>
+                <span className="font-bold">{quantityProducts}</span>
                 sản phẩm
               </span>
             </div>
 
             <p className="flex items-center justify-between">
               <span className="text-[15px] font-normal lg:hidden flex items-center gap-2">
-                <span className="font-bold">
-                  {isLoading ? "Loading..." : data.products.length}
-                </span>
+                <span className="font-bold">{quantityProducts}</span>
                 sản phẩm
               </span>
               <button
@@ -83,28 +106,41 @@ const Collection = () => {
           </div>
           {/* Phần hiển thị sản phẩm */}
           {isLoading ? (
-            <p>Loading Products...</p>
+            <div className="flex flex-wrap mt-4 lg:gap-y-4 gap-y-1 md:gap-y-4"></div>
           ) : (
             <>
               <div className="flex flex-wrap mt-4 lg:gap-y-4 gap-y-1 md:gap-y-4 ">
-                {data && data.products.length > 0
-                  ? data.products.map((product: IProduct) => {
-                      return (
-                        <div
-                          className="lg:flex-[0_0_20%]  lg:px-1.5 px-[2px] md:px-2 lg:max-w-[20%] flex-[0_0_50%] max-w-[50%]"
-                          key={product._id}
-                        >
-                          <CardProduct product={product} />
-                        </div>
-                      );
-                    })
-                  : "No data"}
+                {mergedData && mergedData.products.length > 0 ? (
+                  mergedData.products.map((product: IProduct) => {
+                    return (
+                      <div
+                        className="lg:flex-[0_0_20%]  lg:px-1.5 px-[2px] md:px-2 lg:max-w-[20%] flex-[0_0_50%] max-w-[50%]"
+                        key={product._id}
+                      >
+                        <CardProduct product={product} />
+                      </div>
+                    );
+                  })
+                ) : (
+                  <h3>Không tìm thấy sản phẩm phù hợp</h3>
+                )}
               </div>
+              {hasNextPage && (
+                <div className="flex items-center justify-center">
+                  <button
+                    onClick={() => fetchNextPage()}
+                    disabled={isFetching}
+                    style={{ boxShadow: "0 0 3px rgba(0, 0, 0, 0.08)" }}
+                    className="flex justify-center  text-sm mt-4 transition-all duration-500 hover:bg-red-600 hover:text-white  items-center gap-1 font-medium bg-white rounded-md py-3 px-1.5 min-w-[320px]"
+                  >
+                    Xem thêm <ChevronRightIcon className="size-5" />
+                  </button>
+                </div>
+              )}
             </>
           )}
         </div>
       </section>
-      <Newsletter />
     </Layout>
   );
 };
