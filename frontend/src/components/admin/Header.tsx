d510c66c3302e5b71f3f5c8bf17e37506d33ec9d
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-10 01:18:17 +0700

    Add link

--- frontend/src/components/admin/Header.tsx
+++ frontend/src/components/admin/Header.tsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <ul className="flex items-center justify-center bg-white font-medium gap-14 py-5">
-        <li>Trang chủ</li>
-        <li>Sản phẩm</li>
+      <ul className="flex items-center font-semibold text-[15px] justify-center bg-white gap-14 py-5 border-b border-gray-200 shadow-sm">
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/products">Sản phẩm</Link>
+        </li>
         <li>Bài viết</li>
         <li>Khách hàng</li>
       </ul>
