202c6110ae402d25a6946110075c36121b2a262c
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-10 01:14:37 +0700

    Add middleware auth

--- backend/routes/public/products.js
+++ backend/routes/public/products.js
@@ -6,13 +6,15 @@ import {
   getProducts,
   getProductsByCollectionOrCategory,
 } from "../../controllers/productController.js";
-
+import authMiddleware from "../../middleware/authMiddleware.js";
+import authorizationMiddleware from "../../middleware/authorizationMiddleware.js";
 import upload from "../../middleware/multerMiddleware.js";
 const router = express.Router();
 
 router.get("/collections", getProducts);
 router.get("/collections/:slug", getProductsByCollectionOrCategory);
 router.get("/products/:slug", getProductBySlug);
+router.get("/products", authMiddleware, authorizationMiddleware, getProducts);
 router.post("/product", upload.array("files", 10), addProduct);
 router.delete("/product", deleteProduct);
 export default router;
