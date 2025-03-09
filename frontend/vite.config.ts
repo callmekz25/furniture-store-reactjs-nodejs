7bda9ba585ce00424bbdc07cb17eeab635cde2bd
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-10 01:15:04 +0700

    Rebuild

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  // optimizeDeps: {
+  //   force: true, // Buộc reload lại dependencies
+  // },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
