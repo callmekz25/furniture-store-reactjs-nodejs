6b3139a47e144b8cd8e445e578f4340aedee0287
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-11 13:06:32 +0700

    Add blog router

---
+++ backend/routes/blog.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+  getBlogByCategoryAndSlug,
+  getBlogs,
+  postBlog,
+  uploadImageBlog,
+} from "../controllers/blogController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+import authorizationMiddlware from "../middleware/authorizationMiddleware.js";
+import uploadMiddleware from "../middleware/multerMiddleware.js";
+const router = express.Router();
+
+router.get("/blogs", getBlogs);
+router.get("/blogs/:category/:slug", getBlogByCategoryAndSlug);
+router.post(
+  "/upload-image-blog",
+  authMiddleware,
+  authorizationMiddlware,
+  uploadMiddleware.single("file"),
+  uploadImageBlog
+);
+router.post(
+  "/post-blog",
+  authMiddleware,
+  authorizationMiddlware,
+  uploadMiddleware.single("file"),
+  postBlog
+);
+export default router;
