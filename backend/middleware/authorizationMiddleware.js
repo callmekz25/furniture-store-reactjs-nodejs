a04e9666a7df507ef595f197f2c76edaeaa9e556
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-11 13:05:31 +0700

    Refactor

--- backend/middleware/authorizationMiddleware.js
+++ backend/middleware/authorizationMiddleware.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js";
+import User from "../models/userModel.js";
 const authorizationMiddlware = async (req, res, next) => {
   try {
     if (!req.user?.userId) {
--- backend/routes/public/collection.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getCollections } from "../../controllers/collectionController.js";
-const router = express.Router();
-
-router.get("/get-collections", getCollections);
-
-export default router;
