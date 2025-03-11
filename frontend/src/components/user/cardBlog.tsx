9127296932c50467af3a9916218267b398a373ca
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-11 13:08:14 +0700

    Add blog card t

---
+++ frontend/src/components/user/cardBlog.tsx
@@ -0,0 +1,35 @@
+import IBlog from "@/interfaces/blog.interface";
+import formatDate from "@/utils/formatDate";
+import { CalendarDays, ChevronsRightIcon } from "lucide-react";
+const CardBlog = ({ blog }: { blog: IBlog }) => {
+  return (
+    <div className="flex flex-col gap-2 w-full h-full card-product bg-white rounded transition-all duration-500  overflow-hidden">
+      <img
+        src={blog.thumbnail}
+        alt={blog.title}
+        className="max-w-full object-cotain  hover:cursor-pointer"
+      />
+      <div className="flex flex-col flex-1 py-2.5 px-4">
+        <h3 className="text-[16px] font-bold color-red leading-5 line-clamp-2 hover:cursor-pointer">
+          {blog.title}
+        </h3>
+        <p className="text-sm font-normal line-clamp-2 text-gray-400  overflow-hidden mt-2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam atque
+          autem expedita sapiente porro accusamus fugit libero ipsum, sequi, quo
+          id, maiores velit magnam commodi error at ea ipsam exercitationem.
+        </p>
+        <div className="flex items-center justify-between pt-4 text-gray-400  text-[12px] font-normal border-t border-gray-200 mt-4">
+          <p className="flex items-center gap-1">
+            <CalendarDays className="size-3 " />
+            {formatDate(blog.createdAt)}
+          </p>
+          <button className="flex items-center gap-1 ">
+            Xem thêm <ChevronsRightIcon className="size-3" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardBlog;
