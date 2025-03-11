9b1af55b97e411124f9b7c436adfcb0122f71e10
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-11 13:03:43 +0700

    Add blog routes

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { PORT } from "./constants.js";
 import connectMongo from "./config/db.js";
-import productRoutes from "./routes/public/products.js";
-import authRoutes from "./routes/public/auth.js";
-import reviewRoutes from "./routes/private/review.js";
-import cartRoutes from "./routes/public/cart.js";
-import categoryRoutes from "./routes/public/category.js";
-import collectionRoutes from "./routes/public/collection.js";
+import productRoutes from "./routes/products.js";
+import authRoutes from "./routes/auth.js";
+import reviewRoutes from "./routes/review.js";
+import cartRoutes from "./routes/cart.js";
+import categoryRoutes from "./routes/category.js";
+import collectionRoutes from "./routes/collection.js";
+import blogRoutes from "./routes/blog.js";
 connectMongo();
 const app = express();
 const port = PORT;
@@ -29,6 +30,7 @@ app.use("/v1", reviewRoutes);
 app.use("/v1", cartRoutes);
 app.use("/v1", categoryRoutes);
 app.use("/v1", collectionRoutes);
+app.use("/v1", blogRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
