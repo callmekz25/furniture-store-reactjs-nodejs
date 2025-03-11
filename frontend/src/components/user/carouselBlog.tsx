11e7e8aaa7f2748d07a587771bc3c72113ba6405
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-11 13:08:24 +0700

    Add carousel for blog

---
+++ frontend/src/components/user/carouselBlog.tsx
@@ -0,0 +1,116 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { memo, useRef, useState, useMemo } from "react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
+import useCheckScreen from "@/hooks/useCheckScreen";
+import IBlog from "@/interfaces/blog.interface";
+import CardBlog from "./cardBlog";
+
+const CarouselBlog = ({ blogs, title }: { blogs: IBlog[]; title: string }) => {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const isMobile = useCheckScreen();
+
+  let sliderRef = useRef(null);
+  const next = () => {
+    sliderRef.slickNext();
+  };
+  const previous = () => {
+    sliderRef.slickPrev();
+  };
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    initialSlide: 0,
+    arrows: false, // Ẩn arrow mặc định
+    afterChange: (index: number) => setCurrentIndex(index),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1.22,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  const blogCards = useMemo(() => {
+    return blogs.map((blog, index) => (
+      <div key={`${blog._id}-${index}`} className="lg:px-2 px-1.5 ">
+        <CardBlog blog={blog} />
+      </div>
+    ));
+  }, [blogs]);
+
+  return (
+    <div className="slider-container">
+      <div className="flex items-center justify-between py-5 px-4">
+        <h3 className="font-bold  md:text-2xl text-lg text-red-700">{title}</h3>
+        <div
+          className={` items-center gap-2 ${
+            blogs.length > 4 ? "flex" : "hidden"
+          }`}
+        >
+          <button
+            className={` flex items-center justify-center  bg-gray-200 size-8 rounded-full hover:bg-gray-400 transition-all duration-300 ${
+              currentIndex === 0
+                ? "opacity-40 cursor-not-allowed hover:bg-gray-200"
+                : ""
+            } ${isMobile ? "hidden" : ""}`}
+            onClick={() => previous()}
+            disabled={currentIndex === 0}
+          >
+            <ChevronLeftIcon className="size-5" />
+          </button>
+          <button
+            className={` flex items-center justify-center  bg-gray-200 size-8 rounded-full hover:bg-gray-400 transition-all duration-300 ${
+              currentIndex === blogs.length - 4
+                ? "cursor-not-allowed hover:bg-gray-200 opacity-40"
+                : ""
+            } ${isMobile ? "hidden" : ""}`}
+            onClick={() => next()}
+            disabled={currentIndex === blogs.length - 4}
+          >
+            <ChevronRightIcon className="size-5" />
+          </button>
+        </div>
+      </div>
+      <Slider
+        ref={(slider) => {
+          sliderRef = slider;
+        }}
+        {...settings}
+      >
+        {blogCards}
+      </Slider>
+    </div>
+  );
+};
+
+export default memo(CarouselBlog);
