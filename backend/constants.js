0cbc8479a7c499585fe6ab4a3e2801cbeb051c0c
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-16 23:05:33 +0700

    Constant check production environment

--- backend/constants.js
+++ backend/constants.js
@@ -5,3 +5,4 @@ export const PORT = process.env.PORT || 8080;
 export const JWT_SECRET = process.env.JWT_SECRET_KEY;
 export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
+export const PRODUCTION_ENV = process.env.NODE_ENV === "production";
