2e65127c5509f0b909caaa66975afa73e29ca935
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-17 23:45:38 +0700

    Add connection string atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
-
+import { MONGODB_PASSWORD } from "../constants.js";
 const connectMongo = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/project-fashion");
+    await mongoose.connect(
+      `mongodb+srv://nguyenhongkhanhvinh2511:${MONGODB_PASSWORD}@furniture-store.vvbxd.mongodb.net/furniture-store?retryWrites=true&w=majority&appName=furniture-store`
+    );
+
     console.log("Connect successfully!");
   } catch (error) {
     console.log("Fail to connect!");
