01c35d3821976bdaef218efaa7836b83d9bf12a1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-20 00:58:33 +0700

    Generate slug

---
+++ frontend/src/utils/generateSlug.ts
@@ -0,0 +1,12 @@
+const generateSlug = (name: string): string => {
+  return name
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
+    .trim();
+};
+export default generateSlug;
