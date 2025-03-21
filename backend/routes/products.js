c7b36e7602e6931bf79d58c9dd37b64a954c891c
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-22 00:52:16 +0700

    Add route get related products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,6 +6,7 @@ import {
   getProducts,
   getProductsByCollectionOrCategory,
   getProductsByCollectionWithLimit,
+  getRelatedProducts,
 } from "../controllers/productController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import authorizationMiddleware from "../middleware/authorizationMiddleware.js";
@@ -15,7 +16,8 @@ const router = express.Router();
 router.get("/collections/:slug", getProductsByCollectionOrCategory);
 
 router.get("/products/:slug", getProductBySlug);
-router.get("/products-limit/:slug", getProductsByCollectionWithLimit);
+router.get("/products/collection/:slug", getProductsByCollectionWithLimit);
+router.get("/products/category/:category", getRelatedProducts);
 // router.get("/products", authMiddleware, authorizationMiddleware, getProducts);
 router.get("/products", getProducts);
 router.post("/product", uploadMiddleware, addProduct);
