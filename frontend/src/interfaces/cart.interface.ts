bb0eb2e73e8ec8d77b7878c6f74aa1b45d0f55b2
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-22 00:56:32 +0700

    Update cart interface

--- frontend/src/interfaces/cart.interface.ts
+++ frontend/src/interfaces/cart.interface.ts
@@ -1,5 +1,12 @@
 interface ICart {
   productId: string;
+  slug: string;
+  image: string;
+  discount: number;
+  title: string;
   quantity: number;
+  price: number;
+  fakePrice: number;
+  attributes: string[];
 }
 export default ICart;
