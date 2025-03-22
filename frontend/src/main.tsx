236def3fae95a8c663bc622913949f9b78213273
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-23 01:37:54 +0700

    Config react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,7 +8,19 @@ import { Provider } from "react-redux";
 import { PageProvider } from "./context/cartPageContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+//  Cấu hình QueryClient
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 30, // Giữ cache 30 phút, không fetch lại nếu chưa hết hạn
+      cacheTime: 1000 * 60 * 60, // Dữ liệu cache tồn tại 60 phút trước khi bị xoá
+      refetchOnWindowFocus: false, // Không fetch lại khi chuyển tab
+      refetchOnReconnect: false, // Không fetch lại khi mất mạng rồi có lại
+      refetchOnMount: false, // Không fetch lại khi component mount lại
+      retry: 2, // Retry API tối đa 2 lần nếu lỗi
+    },
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
