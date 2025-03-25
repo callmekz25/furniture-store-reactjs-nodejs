954d34abadd075a95b18823783f4db1d69f7f0ba
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-26 01:11:41 +0700

    Handle authentication

--- frontend/src/redux/actions/auth.action.ts
+++ frontend/src/redux/actions/auth.action.ts
@@ -1,22 +1,17 @@
 import IUser from "@/interfaces/user.interface";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import {
-  handleSignIn,
-  handleSignUp,
-  getUser,
-  handleLogout,
-} from "@/api/authService";
+import { getUser, logout, registerAccount, signIn } from "@/api/authService";
 
 // Hàm thunk để xử lý bất đồng bộ bên ngoài slice
-export const signUpThunk = createAsyncThunk(
+// Reject lỗi sang payload
+export const registerThunk = createAsyncThunk(
   "auth/signup",
   async (user: IUser, { rejectWithValue }) => {
     try {
-      const res = await handleSignUp(user);
+      const res = await registerAccount(user);
       return res;
     } catch (error: any) {
-      // Quăng lỗi từ server qua payload để hiển thị
-      return rejectWithValue(error.response.data.mess);
+      return rejectWithValue(error.response.data);
     }
   }
 );
@@ -24,11 +19,11 @@ export const signInThunk = createAsyncThunk(
   "auth/signin",
   async (user: IUser, { rejectWithValue }) => {
     try {
-      const res = await handleSignIn(user);
+      const res = await signIn(user);
 
       return res;
     } catch (error: any) {
-      return rejectWithValue(error.response.data.mess);
+      return rejectWithValue(error.response.data);
     }
   }
 );
@@ -36,10 +31,10 @@ export const LogoutThunk = createAsyncThunk(
   "auth/logout",
   async (_, { rejectWithValue }) => {
     try {
-      const res = await handleLogout();
+      const res = await logout();
       return res;
     } catch (error: any) {
-      return rejectWithValue(error.response.data.mess);
+      return rejectWithValue(error.response.data);
     }
   }
 );
@@ -51,7 +46,7 @@ export const getUserThunk = createAsyncThunk(
       const res = await getUser();
       return res;
     } catch (error: any) {
-      return rejectWithValue(error.response.data.mess);
+      return rejectWithValue(error.response.data);
     }
   }
 );
