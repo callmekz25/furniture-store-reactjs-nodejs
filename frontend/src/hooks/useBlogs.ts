a5b15da3c159409af0aefc4a3b88bb9474c8ea3c
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:34:46 +0700

    Query get blogs

--- frontend/src/hooks/useBlogs.ts
+++ frontend/src/hooks/useBlogs.ts
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getBlogs, getBlogsContentful } from "@/api/blogService";
+import { getBlogs } from "@/api/blogService";
 const useBlogs = () => {
   return useQuery({
     queryKey: ["blogs"],
-    queryFn: getBlogsContentful,
+    queryFn: getBlogs,
   });
 };
 export default useBlogs;
