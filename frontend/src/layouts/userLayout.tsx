7fe68c4e7a7f57207f561a1fb8604a3cadd47214
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:36:23 +0700

    Default layout

--- frontend/src/layouts/userLayout.tsx
+++ frontend/src/layouts/userLayout.tsx
@@ -2,15 +2,15 @@ import Header from "../components/user/header";
 import Footer from "../components/user/footer";
 import { Toaster } from "@/components/ui/sonner";
 import Newsletter from "@/components/user/newsLetter";
+import { Outlet } from "react-router-dom";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout = () => {
   return (
     <>
       <Header />
-      <main className="bg-[#f7f7f7]">{children}</main>
+      <main className="bg-[#f7f7f7] min-h-screen">
+        <Outlet />
+      </main>
       <Toaster />
       <Newsletter />
       <Footer />
