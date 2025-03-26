0589d2779142b6f6e48babdf79da05ee0d8bfcb4
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:42:53 +0700

    Check user is authenticated

--- frontend/src/routes/protectedRoute.tsx
+++ frontend/src/routes/protectedRoute.tsx
@@ -1,26 +1,18 @@
-import Loading from "@/components/user/loading";
 import { useAppSelector } from "@/redux/hook";
 import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  const navigate = useNavigate();
-  const { user, loading } = useAppSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (!user) {
-      navigate("/signin", { replace: true });
-    }
-  }, [user, navigate]);
-
-  if (loading) {
-    return <Loading />;
+  const { user, isAuthenticated } = useAppSelector((state) => state.auth);
+  if (!user || !isAuthenticated) {
+    return <Navigate to="/signin" />;
   }
-  return user ? <Outlet /> : null;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
