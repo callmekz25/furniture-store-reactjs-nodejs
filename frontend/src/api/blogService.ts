4554158b0b1735899c10c1dd23ac3524600657fd
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:28:34 +0700

    Update get blogs with necessary fields

--- frontend/src/api/blogService.ts
+++ frontend/src/api/blogService.ts
@@ -1,91 +1,71 @@
-import IBlog from "@/interfaces/blog.interface";
-import httpRequest, { httpContentful } from "./config";
-
-const getBlogs = async () => {
-  try {
-    const { data } = await httpRequest.get("/blogs");
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+import { httpContentful } from "./config";
 
 const getBlogByCategoryAndSlug = async (slug: string, category: string) => {
   try {
-    const { data } = await httpRequest.get(`/blogs/${category}/${slug}`);
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const uploadImageToCloudinary = async (file: File) => {
-  try {
-    const formData = new FormData();
-    formData.append("file", file);
-    const { data } = await httpRequest.post("/upload-image-blog", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
+    const response = await httpContentful.get(`/entries`, {
+      params: {
+        content_type: "blog",
+        include: 1,
+        "fields.categorySlug": category,
+        "fields.slug": slug,
+      },
     });
-    return data;
+    const items = response.data.items;
+    const assets = response.data.includes?.Asset || [];
+    // Lấy ra phần tử đầu luôn để biến thành object thay vì response mặc đinh là 1 mảng
+    const item = items[0];
+
+    const thumbnailId = item.fields.thumbnail?.sys.id;
+    const thumbnailAsset = assets.find(
+      (asset: []) => asset.sys.id === thumbnailId
+    );
+
+    return {
+      ...item.fields,
+      assets,
+      id: item.sys.id,
+      thumbnailUrl: thumbnailAsset?.fields.file.url || null,
+    };
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
 
-const postBlog = async (file: File, blog: IBlog) => {
-  try {
-    const { title, content, publish, category, tags, slug } = blog;
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("title", String(title));
-    formData.append("content", String(content));
-    formData.append("publish", String(publish));
-    formData.append("category", String(category));
-    formData.append("tags", JSON.stringify(tags));
-    formData.append("slug", String(slug));
-    const { data } = await httpRequest.post("/post-blog", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getBlogsContentful = async () => {
+const getBlogs = async () => {
   try {
     const response = await httpContentful.get(`/entries`, {
-      params: { content_type: "blog", include: 2 }, // Thay "blog" bằng Content Type ID của bạn
+      params: {
+        content_type: "blog",
+        include: 1,
+        limit: 6,
+        select:
+          "sys,fields.title,fields.slug,fields.thumbnail,fields.categorySlug,fields.description,fields.createdAt",
+      },
     });
     const items = response.data.items;
     const assets = response.data.includes?.Asset || []; // Danh sách assets
-    console.log(response);
 
     // Lặp qua từng blog và gán URL ảnh
-    const blogs = items.map((item) => {
+    const blogs = items.map((item: any) => {
       const thumbnailId = item.fields.thumbnail?.sys.id;
+      if (!thumbnailId) {
+        console.warn("Không tìm thấy thumbnailId cho item:", item);
+      }
       const thumbnailAsset = assets.find(
-        (asset) => asset.sys.id === thumbnailId
+        (asset: any) => asset.sys.id === thumbnailId
       );
 
       return {
         ...item.fields,
         assets,
         id: item.sys.id,
-        thumbnailUrl: thumbnailAsset?.fields.file.url || null, // Lấy URL ảnh
+        thumbnailUrl: thumbnailAsset?.fields.file.url || null,
       };
     });
 
-    console.log("Blogs:", blogs);
     return blogs;
   } catch (error) {
     throw new Error(error);
   }
 };
-export {
-  getBlogsContentful,
-  getBlogs,
-  uploadImageToCloudinary,
-  postBlog,
-  getBlogByCategoryAndSlug,
-};
+export { getBlogs, getBlogByCategoryAndSlug };
