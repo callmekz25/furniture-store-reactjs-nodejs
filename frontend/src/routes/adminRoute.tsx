41db6f358f0592c0ae57b6f113d5bc0b254bf1e7
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:41:57 +0700

    Check user is authenticated and role's user

--- frontend/src/routes/adminRoute.tsx
+++ frontend/src/routes/adminRoute.tsx
@@ -1,31 +1,19 @@
 import { useAppSelector } from "@/redux/hook";
-import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import { Outlet, Navigate } from "react-router-dom";
 
 const AdminRoute = () => {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  const navigate = useNavigate();
-  const { user, loading } = useAppSelector((state) => state.auth);
-  useEffect(() => {
-    // Chỉ xử lý chuyển hướng khi đã tải xong dữ liệu
-    if (!loading) {
-      if (!user) {
-        navigate("/signin", { replace: true });
-      } else if (user.role !== "admin") {
-        navigate("/", { replace: true });
-      }
-    }
-  }, [user, loading, navigate]);
+  const { user, isAuthenticated } = useAppSelector((state) => state.auth);
 
-  // Hiển thị loading hoặc null trong khi đang tải
-  if (loading) {
-    return <div>Loading...</div>;
+  if (!user || !isAuthenticated) {
+    return <Navigate to="/signin" />;
   }
 
-  return user?.role === "admin" ? <Outlet /> : null;
+  if (user.role !== "admin") {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
 };
 
 export default AdminRoute;
