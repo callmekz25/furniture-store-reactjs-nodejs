545192f700ed6f6a7d0c64409dd3d9efc50c2516
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-27 00:29:42 +0700

    Render with data from contentful api

--- frontend/src/components/user/blogCard.tsx
+++ frontend/src/components/user/blogCard.tsx
@@ -6,26 +6,22 @@ const CardBlog = ({ blog }: { blog: IBlog }) => {
   return (
     <div className="flex flex-col gap-2 w-full h-full card-product bg-white rounded transition-all duration-500  overflow-hidden">
       <Link
-        to={`/blogs/${blog.category}/${blog.slug}`}
+        to={`/blogs/${blog.categorySlug}/${blog.slug}`}
         className=" overflow-hidden"
       >
         <img
           src={blog.thumbnailUrl}
           alt={blog.title}
           loading="lazy"
-          width={334}
-          height={334}
-          className="max-w-full object-cover w-full h-full  hover:cursor-pointer transition-all duration-500 hover:-rotate-2 hover:scale-110"
+          className="max-w-full object-cover w-full h-full  hover:cursor-pointer transition-all aspect-[1/1] duration-500 hover:-rotate-2 hover:scale-110"
         />
       </Link>
       <div className="flex flex-col flex-1 py-2.5 px-4 ">
         <h3 className="text-[16px] min-h-[40px] font-bold color-red leading-5 line-clamp-2 hover:cursor-pointer">
           {blog.title}
         </h3>
-        <p className="text-sm font-normal line-clamp-2 text-gray-400  overflow-hidden mt-2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam atque
-          autem expedita sapiente porro accusamus fugit libero ipsum, sequi, quo
-          id, maiores velit magnam commodi error at ea ipsam exercitationem.
+        <p className="text-sm font-normal line-clamp-2 text-gray-400 mt-2">
+          {blog.description}
         </p>
         <div className="flex items-center justify-between pt-4 text-gray-400  text-[12px] font-normal border-t border-gray-200 mt-4">
           <p className="flex items-center gap-1">
@@ -33,7 +29,7 @@ const CardBlog = ({ blog }: { blog: IBlog }) => {
             {formatDate(blog.createdAt)}
           </p>
           <Link
-            to={`/blogs/${blog.category}/${blog.slug}`}
+            to={`/blogs/${blog.categorySlug}/${blog.slug}`}
             className="flex items-center gap-1 "
           >
             Xem thêm <ChevronsRightIcon name="Xem thêm" className="size-3" />
