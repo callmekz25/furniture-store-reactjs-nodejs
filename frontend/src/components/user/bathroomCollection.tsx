b35c23a34807205918e42e817e138210fa995cb1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-28 01:37:58 +0700

    Add bathroom collections

---
+++ frontend/src/components/user/bathroomCollection.tsx
@@ -0,0 +1,49 @@
+import useProductsByCollection from "@/hooks/useProductsByCollection";
+import Image from "../../assets/background.webp";
+import CarouselBathroomProducts from "./carouselBathroomProducts";
+import Loading from "./loading";
+import { useMemo } from "react";
+const BathroomCollection = () => {
+  const { data, isLoading, error } = useProductsByCollection(
+    "phu-kien-phong-tam",
+    9
+  );
+  const products = useMemo(() => {
+    const result = [];
+    if (data && data.length > 0) {
+      for (let index = 0; index < data.length; index += 3) {
+        result.push(data.slice(index, index + 3));
+      }
+    }
+    return result;
+  }, [data]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <p>Lỗi</p>;
+  }
+  return (
+    <div className="pb-[70px]">
+      <div className="flex bg-white flex-wrap">
+        <div className="lg:flex-[0_0_70%] lg:max-w-[70%]  flex-[0_0_100%] max-w-full pt-5 pr-5 pl-4 flex flex-col">
+          <CarouselBathroomProducts
+            products={products}
+            title="Chút xinh xắn cho nhà tắm"
+          />
+        </div>
+        <div className="lg:flex-[0_0_30%] lg:max-w-[30%]  flex-[0_0_100%] max-w-full">
+          <img
+            src={Image}
+            alt=""
+            loading="lazy"
+            className="max-w-full object-cover h-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BathroomCollection;
