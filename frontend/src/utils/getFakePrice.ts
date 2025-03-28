67d0d0ba5198da068df90bbfd7a92780ee35e332
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-28 20:42:58 +0700

    Get fake price with product have variants & without variants

--- frontend/src/utils/getFakePrice.ts
+++ frontend/src/utils/getFakePrice.ts
@@ -1,17 +1,16 @@
 import IProduct from "@/interfaces/product.interface";
-import formatPriceToVND from "./formatPriceToVND";
 
 const getFakePrice = (product: IProduct) => {
   if (product.discount && product.discount > 0) {
     if (product.variants && product.variants.length > 0) {
-      return formatPriceToVND(product.variants[0].fakePrice);
+      return product.variants[0].fakePrice;
     } else {
       if (product.fakePrice > 0) {
-        return formatPriceToVND(product.fakePrice);
+        return product.fakePrice;
       }
-      return "";
+      return 0;
     }
   }
-  return "";
+  return 0;
 };
 export default getFakePrice;
