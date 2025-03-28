3b832d64624300e2f21683862bccbec867b0dd25
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-03-28 20:38:52 +0700

    Add button previous & next slides

--- frontend/src/components/user/Banner.tsx
+++ frontend/src/components/user/Banner.tsx
@@ -1,14 +1,10 @@
-import Image from "../../assets/slide.avif";
-import Image2 from "../../assets/sale.jpg";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import useBannersByType from "@/hooks/useBannersByType";
 import Loading from "./loading";
+import { Link } from "react-router-dom";
 const Banner = () => {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
   const { data, isLoading, error } = useBannersByType("hero");
   let sliderRef = useRef(null);
   const next = () => {
@@ -30,13 +26,18 @@ const Banner = () => {
     autoplay: true,
     autoplaySpeed: 2500,
     waitForAnimate: false,
-    afterChange: (index: number) => setCurrentIndex(index),
   };
   if (isLoading) {
     return <Loading />;
   }
   return (
-    <div className="">
+    <div className=" relative">
+      <button
+        onClick={() => next()}
+        className=" absolute top-1/2 right-5 flex items-center justify-center z-10 -translate-y-1/2 size-10 bg-red-700"
+      >
+        <ChevronRightIcon className="size-6 text-white" />
+      </button>
       <Slider
         ref={(slider) => {
           sliderRef = slider;
@@ -45,16 +46,26 @@ const Banner = () => {
       >
         {data.map((item) => {
           return (
-            <div className="w-full" key={item._id}>
+            <Link
+              to={`${item.slug !== "" ? `/collections/${item.slug}` : "/"}`}
+              className="w-full"
+              key={item._id}
+            >
               <img
                 src={item.image}
                 alt={item.name}
                 className="max-w-full object-cover"
               />
-            </div>
+            </Link>
           );
         })}
       </Slider>
+      <button
+        onClick={() => previous()}
+        className=" absolute top-1/2 left-5 flex items-center justify-center z-10 -translate-y-1/2 size-10 bg-red-700"
+      >
+        <ChevronLeftIcon className="size-6 text-white" />
+      </button>
     </div>
   );
 };
