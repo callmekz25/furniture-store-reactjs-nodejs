d36aeac7753ac625c640c693fb21eb6c61a835b7
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-11 00:27:47 +0700

    Refactor

--- frontend/src/hooks/useProductsByCollection.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getProductsByCollection } from "@/api/productService";
-const useProductsByCollection = (slug: string, limit?: number) => {
-  return useQuery({
-    queryKey: ["products", slug, limit],
-    queryFn: () => getProductsByCollection(slug, limit),
-  });
-};
-export default useProductsByCollection;
--- frontend/src/hooks/useRelatedProductsBySlug.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getRelatedProducts } from "@/api/productService";
-const useRelatedProductsBySlug = (slug: string, limit?: number) => {
-  return useQuery({
-    queryKey: ["relatedProducts", slug, limit],
-    queryFn: () => getRelatedProducts(slug, limit),
-  });
-};
-export default useRelatedProductsBySlug;
