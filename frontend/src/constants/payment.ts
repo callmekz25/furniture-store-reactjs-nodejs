7b01778326b044df2212b53ea05b5c36bd972724
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-12 23:57:30 +0700

    Add constants payments method

---
+++ frontend/src/constants/payment.ts
@@ -0,0 +1,18 @@
+import Momo from "../assets/images/wallets/momo.svg";
+import Banking from "../assets/images/wallets/banking.svg";
+import Cod from "../assets/images/wallets/cod.svg";
+const PAYMENTS = [
+  {
+    label: "Thanh toán khi nhận hàng (COD)",
+    image: Cod,
+  },
+  {
+    label: "Chuyển khoản qua ngân hàng",
+    image: Banking,
+  },
+  {
+    label: "Ví Momo",
+    image: Momo,
+  },
+];
+export default PAYMENTS;
