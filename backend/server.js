34211ed432eb12746e249369afdd5e2d9e3214dd
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-13 22:53:25 +0700

    Add payment routes

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ import collectionRoutes from "./routes/collection.js";
 import blogRoutes from "./routes/blog.js";
 import bannerRoutes from "./routes/banner.js";
 import orderRoutes from "./routes/order.js";
+import paymentRoutes from "./routes/payment.js";
 connectMongo();
 const app = express();
 const port = PORT;
@@ -38,6 +39,7 @@ app.use("/v1", collectionRoutes);
 app.use("/v1", blogRoutes);
 app.use("/v1", bannerRoutes);
 app.use("/v1", orderRoutes);
+app.use("/v1", paymentRoutes);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
