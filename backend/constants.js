83cc40cff030f22f4b601db4db7b8bc8da781760
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-13 22:53:03 +0700

    Add momo key

--- backend/constants.js
+++ backend/constants.js
@@ -8,3 +8,6 @@ export const REFRESH_TOKEN = "refreshToken";
 export const PRODUCTION_ENV = process.env.NODE_ENV === "production";
 export const LIMIT = 20;
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+export const MOMO_ACCESS_KEY = process.env.MOMO_ACCESS_KEY;
+export const MOMO_SECRET_KEY = process.env.MOMO_SECRET_KEY;
+export const MOMO_URL = process.env.MOMO_URL;
