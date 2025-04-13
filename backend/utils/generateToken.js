946c39da77c8edc3995e0bf138d6f9c796f28da7
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-13 22:56:00 +0700

    Use object id

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../constants.js";
 const generateAccessToken = (user) => {
   // Lần đầu đăng nhập sẽ user._id các lần sau truy cập token sẽ là userId
-  const userId = user._id ? user._id.toString() : user.userId;
+  const userId = user._id ? user._id : user.userId;
   return jwt.sign(
     { userId: userId, name: user.name, email: user.email },
     JWT_SECRET,
@@ -12,7 +12,7 @@ const generateAccessToken = (user) => {
   );
 };
 const generateRefreshToken = (user) => {
-  const userId = user._id ? user._id.toString() : user.userId;
+  const userId = user._id ? user._id : user.userId;
   return jwt.sign(
     { userId: userId, name: user.name, email: user.email },
     JWT_SECRET,
