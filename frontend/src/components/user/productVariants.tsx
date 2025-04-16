91f1ea5d8f57b5134fc07ce5707b8188f0cee65e
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-16 22:45:30 +0700

    add component product variants

--- frontend/src/components/user/productVariants.tsx
+++ frontend/src/components/user/productVariants.tsx
@@ -14,6 +14,7 @@ const ProductVariants = ({
   onSelectVariant,
 }: {
   variants: IVariants[];
+  onSelectVariant: () => void;
 }) => {
   const [selectedAttributes, setSelectedAttributes] = useState({});
 
