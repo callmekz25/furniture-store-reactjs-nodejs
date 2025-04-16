2022ad2423f648423d68323aec2390b2644da60f
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-16 22:22:06 +0700

    style: change width

--- frontend/src/layouts/adminLayout.tsx
+++ frontend/src/layouts/adminLayout.tsx
@@ -2,9 +2,9 @@ import Header from "../components/admin/Header";
 import { Outlet } from "react-router-dom";
 const LayoutAdmin = () => {
   return (
-    <div className=" bg-white">
+    <div className="bg-white ">
       <Header />
-      <main className="py-10 max-w-[1200px] min-h-screen mx-auto">
+      <main className=" py-10  min-h-screen max-w-[1300px] mx-auto">
         <Outlet />
       </main>
     </div>
