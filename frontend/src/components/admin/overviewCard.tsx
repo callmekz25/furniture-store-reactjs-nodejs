0e49c5b1eaf8201c2d1938066cb01a87c7b0e564
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-16 22:18:45 +0700

    style: change bacground color

--- frontend/src/components/admin/overviewCard.tsx
+++ frontend/src/components/admin/overviewCard.tsx
@@ -8,12 +8,12 @@ const OverviewCard = ({
   total: number;
 }) => {
   return (
-    <div className="flex-1 rounded-xl  bg-white p-5 flex flex-col gap-4">
+    <div className="flex-1 rounded-lg border border-gray-200 shadow  bg-white p-5 flex flex-col gap-4">
       <div className="flex flex-col gap-1">
         <h3 className=" text-lg font-bold">{title}</h3>
         <p className="text-sm font-medium text-gray-500">{descr}</p>
       </div>
-      <h4 className="text-3xl font-bold mt-2">{total}</h4>
+      <h4 className="text-4xl font-bold mt-2">{total}</h4>
     </div>
   );
 };
