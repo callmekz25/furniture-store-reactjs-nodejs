aa16f5decc2e528902545c67e615ea3a869775a1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-16 22:45:45 +0700

    add hook for get product by id

--- frontend/src/hooks/product/useProductById.ts
+++ frontend/src/hooks/product/useProductById.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductById } from "@/api/productService";
+
 const useProductById = (productId: string) => {
   return useQuery({
     queryKey: ["products", productId],
