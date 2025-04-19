8a164e2c872f0cb60a9c5adff519fe98c9da5067
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-20 01:28:08 +0700

    add contact info

---
+++ frontend/src/constants/contacts.tsx
@@ -0,0 +1,17 @@
+import { HeadphonesIcon, StoreIcon, TruckIcon } from "lucide-react";
+
+const CONTACTS = [
+  {
+    label: "Giao hàng toàn quốc",
+    icon: <TruckIcon size={20} />,
+  },
+  {
+    label: "Hệ thống cửa hàng",
+    icon: <StoreIcon size={20} />,
+  },
+  {
+    label: "Hotline: 0899348258",
+    icon: <HeadphonesIcon size={20} />,
+  },
+];
+export default CONTACTS;
