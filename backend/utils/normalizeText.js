6bf19aa3453255599699e399a5bc7172330e90f5
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-20 01:22:41 +0700

    add utils normalizeText function to remove accents from Vietnamese text

---
+++ backend/utils/normalizeText.js
@@ -0,0 +1,8 @@
+const normalizeText = (text) => {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D");
+};
+export default normalizeText;
