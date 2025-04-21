5d38d9d381d4c56a038b8f1de38513ea9976f00b
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:33:30 +0700

    add route to create payment with momo

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { createMoMoPayment } from "../controllers/paymentController.js";
+import { createMoMoPayment } from "../controllers/payment.controller.js";
 const router = express.Router();
 
 router.post("/payment/momo", createMoMoPayment);
