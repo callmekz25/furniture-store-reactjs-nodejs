71b225b6e3c6a1451e7e1259ccf0fea66d49dd39
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:33:18 +0700

    add route to create order & get checkout

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,17 +1,17 @@
 import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
-import validateCheckoutParam from "../middleware/verifyCheckoutParamMiddleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
+import verifyCheckoutParam from "../middleware/verifyCheckoutParam.middleware.js";
 import {
   createOrderDraft,
   getCheckoutOrder,
-} from "../controllers/orderController.js";
+} from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.get(
   "/checkouts/:orderId",
   authMiddleware,
-  validateCheckoutParam,
+  verifyCheckoutParam,
   getCheckoutOrder
 );
 router.post("/checkouts", authMiddleware, createOrderDraft);
