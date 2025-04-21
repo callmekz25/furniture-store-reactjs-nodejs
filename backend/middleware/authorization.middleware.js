85978d992c2943dcedcb8cf90c6d95546ad04738
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:22:37 +0700

    feat check role of user

---
+++ backend/middleware/authorization.middleware.js
@@ -0,0 +1,23 @@
+import User from "../models/user.model.js";
+const authorizationMiddlware = async (req, res, next) => {
+  try {
+    if (!req.user?.userId) {
+      return res.status(401).json({ mess: "Unauthorized:" });
+    }
+
+    const user = await User.findById(req.user.userId).select("role");
+    if (!user) {
+      return res.status(401).json({ mess: "Unauthorized" });
+    }
+
+    if (user.role !== "admin") {
+      return res.status(403).json({ mess: "Forbidden" });
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ mess: "Internal Server Error" });
+  }
+};
+
+export default authorizationMiddlware;
