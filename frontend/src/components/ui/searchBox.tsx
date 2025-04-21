8ac48d395fa5fe6551e12552c45863769e42d569
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:40:39 +0700

    feat add clear search value when navigate

--- frontend/src/components/ui/searchBox.tsx
+++ frontend/src/components/ui/searchBox.tsx
@@ -6,17 +6,20 @@ import IProduct from "@/interfaces/product.interface";
 import formatPriceToVND from "@/utils/formatPriceToVND";
 import getProductImages from "@/utils/getProductImages";
 import getFakePrice from "@/utils/getFakePrice";
+import { Link, useLocation } from "react-router-dom";
+
 const SearchBox = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const location = useLocation();
   const debounceSearchTermValue = useDebounce(searchTerm, 500);
 
   const { data, isLoading, error } = useProductsBySearch(
     debounceSearchTermValue
   );
-  useEffect(() => {
-    console.log(debounceSearchTermValue);
-  }, [debounceSearchTermValue]);
 
+  useEffect(() => {
+    setSearchTerm("");
+  }, [location.pathname]);
   return (
     <>
       <div className=" w-[60%] mx-auto flex flex-col ">
@@ -25,6 +28,7 @@ const SearchBox = () => {
             type="text"
             className=" rounded outline-none text-sm font-medium text-black bg-white w-full py-2.5 pl-4 pr-13 placeholder:text-sm placeholder:font-normal"
             placeholder="Tìm kiếm sản phẩm..."
+            value={searchTerm}
             onChange={(e) => {
               setSearchTerm(e.target.value);
             }}
@@ -33,21 +37,26 @@ const SearchBox = () => {
             <SearchIcon className="size-5  text-white" />
           </button>
           <div
-            className={`absolute px-5 top-full left-0 w-full bg-white z-[999] h-auto transition-opacity duration-500 ${
+            className={`absolute px-5 top-full shadow-md left-0 w-full bg-white z-[999] h-auto transition-opacity duration-500 ${
               debounceSearchTermValue && data
                 ? "opacity-100 visible pointer-events-auto"
                 : "opacity-0 invisible pointer-events-none"
             }`}
           >
-            {data && data.products && data.products.length > 0 ? (
-              data.products.map((product: IProduct) => {
+            {data && data.data.products && data.data.products.length > 0 ? (
+              data.data.products.map((product: IProduct) => {
                 return (
                   <div
                     key={product._id}
                     className="py-4 flex items-center justify-between border-b border-gray-200"
                   >
                     <div className="flex flex-col text-black text-sm font-normal opacity-80">
-                      <span>{product.title}</span>
+                      <Link
+                        onClick={() => setSearchTerm("")}
+                        to={`/products/${product.slug}`}
+                      >
+                        {product.title}
+                      </Link>
                       <div className="flex items-center gap-2">
                         <span className="font-medium text-black">
                           {formatPriceToVND(product.minPrice)}
@@ -74,7 +83,7 @@ const SearchBox = () => {
                 Không có sản phẩm nào
               </div>
             )}
-            {data && data.products && data.products.length > 0 && (
+            {data && data.data.products && data.data.products.length > 0 && (
               <button className="text-sm font-normal hover:text-red-700 w-full text-black opacity-70 py-4 flex items-center justify-center">
                 Xem thêm {data.total} sản phẩm
               </button>
