6d71de550e3feef376f4bfaedb234ba09eb6aa43
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:18:13 +0700

    rename file

--- backend/controllers/accountController.js
+++
@@ -1,9 +0,0 @@
-const getAccount = async (req, res) => {
-  try {
-    const user = req.user;
-    return res.status(200).json(user);
-  } catch (error) {
-    return res.status(500).json({ mess: error.message });
-  }
-};
-export { getAccount };
