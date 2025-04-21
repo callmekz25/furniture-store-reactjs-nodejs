09a3b017a53b1fd43a24c4341eec47a447bce646
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:19:56 +0700

    category controller

---
+++ backend/controllers/category.controller.js
@@ -0,0 +1,14 @@
+import Category from "../models/category.model.js";
+
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    if (!categories) {
+      return res.status(404).json({ mess: "Not found" });
+    }
+    return res.status(200).json(categories);
+  } catch (error) {
+    return res.status(500).json({ mess: error.message });
+  }
+};
+export { getCategories };
