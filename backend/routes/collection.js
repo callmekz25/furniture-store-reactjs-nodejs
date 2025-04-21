77849a4ebef4ca5aaace3b2fa858c16d97582de6
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-21 23:32:47 +0700

    add route to get all collections

--- backend/routes/collection.js
+++ backend/routes/collection.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCollections } from "../controllers/collectionController.js";
+import { getCollections } from "../controllers/collection.controller.js";
 const router = express.Router();
 
 router.get("/get-collections", getCollections);
