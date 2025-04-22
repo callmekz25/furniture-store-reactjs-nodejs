428d7b986274a692ac955e74fd135b9b70d4e3b0
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-23 00:12:31 +0700

    fix use auth controller

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,15 @@
 import express from "express";
-import {
-  signIn,
-  signUp,
-  logOut,
-  getUser,
-} from "../controllers/auth.controller.js";
+import AuthController from "../controllers/auth.controller.js";
 import authMiddleware from "../middleware/auth.middleware.js";
+import optionalMiddleware from "../middleware/optionalAuth.middleware.js";
+import TokenController from "../controllers/token.controller.js";
 const router = express.Router();
-
-router.get("/get-user", authMiddleware, getUser);
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-router.post("/logout", logOut);
+// TODO: Handle refresh token, axios don't refresh token
+// ERROR
+router.get("/get-user", optionalMiddleware, AuthController.getUser);
+router.post("/signup", AuthController.register);
+router.post("/refresh-token", TokenController.refreshToken);
+router.post("/signin", AuthController.login);
+router.post("/logout", AuthController.logout);
 
 export default router;
