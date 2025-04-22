629f91200418cb656d519f0e34b1101bc3152a70
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-23 00:12:16 +0700

    feat add optional authentication middleware for check user is login or guest

---
+++ backend/middleware/optionalAuth.middleware.js
@@ -0,0 +1,50 @@
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN, JWT_SECRET } from "../constants.js";
+
+const optionalMiddleware = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!token && !refreshToken) {
+    req.user = null;
+    return next();
+  }
+
+  // Check and verify access token
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    req.user = user;
+    return next();
+  } catch (err) {
+    // Access token is not valid or expired
+    if (!refreshToken) {
+      req.user = null;
+
+      return next();
+    }
+
+    try {
+      // refresh token is valid, create new access token
+      const user = jwt.verify(refreshToken, JWT_SECRET);
+      const newAccessToken = jwt.sign(user, JWT_SECRET, {
+        expiresIn: "15m",
+      });
+      console.log("New access token");
+
+      res.cookie(ACCESS_TOKEN, newAccessToken, {
+        httpOnly: true,
+        secure: false,
+        sameSite: "lax",
+        maxAge: 15 * 60 * 1000,
+      });
+
+      req.user = user;
+      return next();
+    } catch (refreshErr) {
+      req.user = null;
+      return next();
+    }
+  }
+};
+
+export default optionalMiddleware;
