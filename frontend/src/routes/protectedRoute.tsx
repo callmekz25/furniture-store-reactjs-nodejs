95fbeed525173bbf0e3e773922ca9571f9ef8efd
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-23 23:01:57 +0700

    fix get user of data to check user is login

--- frontend/src/routes/protectedRoute.tsx
+++ frontend/src/routes/protectedRoute.tsx
@@ -1,14 +1,17 @@
-import { useAppSelector } from "@/redux/hook";
 import { useEffect } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-
+import useUser from "@/hooks/auth/useAuth";
+import Loading from "@/components/loading/loading";
 const ProtectedRoute = () => {
   const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
-  const { user, isAuthenticated } = useAppSelector((state) => state.auth);
-  if (!user || !isAuthenticated) {
+  const { data: user, isLoading, error } = useUser();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (!user) {
     return <Navigate to="/signin" />;
   }
 
