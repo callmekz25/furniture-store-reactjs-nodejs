672ffbf7a6dc2ebf60cd4133f424f97994a81d4d
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:02:22 +0700

    feat add constant for user name of mongodb

--- backend/constants.js
+++ backend/constants.js
@@ -7,6 +7,7 @@ export const ACCESS_TOKEN = "accessToken";
 export const REFRESH_TOKEN = "refreshToken";
 export const PRODUCTION_ENV = process.env.NODE_ENV === "production";
 export const LIMIT = 20;
+export const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 export const MOMO_ACCESS_KEY = process.env.MOMO_ACCESS_KEY;
 export const MOMO_SECRET_KEY = process.env.MOMO_SECRET_KEY;
