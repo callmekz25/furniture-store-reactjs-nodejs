820142451c1bd146dbd95e6c871f26638c6ed772
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-04-26 22:02:42 +0700

    fix handle log error for connect db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
-import { MONGODB_PASSWORD } from "../constants.js";
+import { MONGODB_PASSWORD, MONGODB_USERNAME } from "../constants.js";
 const connectMongo = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://nguyenhongkhanhvinh2511:${MONGODB_PASSWORD}@furniture-store.vvbxd.mongodb.net/furniture-store?retryWrites=true&w=majority&appName=furniture-store`
+      `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@furniture-store.vvbxd.mongodb.net/furniture-store?retryWrites=true&w=majority&appName=furniture-store`
     );
 
     console.log("Connect successfully!");
   } catch (error) {
+    console.log(error.message);
+
     console.log("Fail to connect!");
   }
 };
