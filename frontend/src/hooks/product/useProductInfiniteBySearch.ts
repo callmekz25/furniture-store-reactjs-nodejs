a571e6b79af6589ed6288c86ea457790226cb341
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-02 17:37:09 +0700

    fix get next page

--- frontend/src/hooks/product/useProductInfiniteBySearch.ts
+++ frontend/src/hooks/product/useProductInfiniteBySearch.ts
@@ -1,10 +1,22 @@
+import { getProductsBySearchTerm } from "@/api/productService";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
-const useProductInfiniteBySearch = (query: string, all: boolean = false) => {
-  return useInfiniteQuery(
-    {
-      queryKey: ["search", query, all],
-      queryFn: ({pageParam})
-    }
-  )
+const useProductInfiniteBySearch = (query: string) => {
+  return useInfiniteQuery({
+    queryKey: ["search", query, "all"],
+    enabled: !!query,
+    queryFn: ({ pageParam }) => getProductsBySearchTerm(query, pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      // lastPage là object chứa response api mỗi lần trả về
+      // allPage là array chứa các lastPage của tất cẩ response api trả về
+
+      const limit = import.meta.env.VITE_LIMIT;
+
+      return lastPage.products.length === Number(limit)
+        ? allPages.length + 1
+        : undefined;
+    },
+  });
 };
+export default useProductInfiniteBySearch;
