922b0b17ddaf6a671a670cf1866aeb4258266aad
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-05-02 17:33:34 +0700

    add custom response

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 import Order from "../models/order.model.js";
 import asyncHandler from "../helpers/asyncHandler.js";
-
+import { OkSuccess } from "../core/success.response.js";
 const getCheckoutOrder = asyncHandler(async (req, res, next) => {
   const order = req.order;
   if (!order) {
     return res.status(404).json({ mess: "Not found order" });
   }
-  return res.status(200).json(order);
+  return res.status(200).json(new OkSuccess({ data: order }));
 });
 
 const createOrderDraft = asyncHandler(async (req, res, next) => {
@@ -19,22 +19,52 @@ const createOrderDraft = asyncHandler(async (req, res, next) => {
 
   if (req.user) {
     orderTemp = new Order({
+      order_code: "31258",
       userId: req.user.userId,
+      order_info: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        note: "",
+        address: "",
+        province: "",
+        district: "",
+        ward: "",
+      },
       products: products,
-      note: note ?? "",
+      order_status: "pending",
+      payment: {
+        payment_status: false,
+      },
       total_price,
       total_items,
     });
   } else {
     orderTemp = new Order({
+      order_code: "31258",
+      order_info: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        note: "",
+        address: "",
+        province: "",
+        district: "",
+        ward: "",
+      },
       products: products,
-      note: note ?? "",
+      order_status: "pending",
+      payment: {
+        payment_status: false,
+      },
       total_price,
       total_items,
     });
   }
 
   await orderTemp.save();
-  return res.status(200).json({ orderId: orderTemp._id.toString() });
+  return res
+    .status(200)
+    .json(new OkSuccess({ data: { orderId: orderTemp._id.toString() } }));
 });
 export { getCheckoutOrder, createOrderDraft };
