9b5e338d8a3c2725d9b07fb4bb190df424a36d94
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-06-29 21:58:17 +0700

    fix warning type

--- frontend/src/utils/getProductImages.ts
+++ frontend/src/utils/getProductImages.ts
@@ -6,31 +6,34 @@ const getProductImages = (
 ): string | string[] => {
   if (onlyOneImage) {
     if (product?.images?.length > 0) {
-      return product.images.slice(0, 1)[0];
+      return product.images.slice(0, 1)[0] as string;
     }
     if (product?.variants?.length > 0) {
-      return product.variants[0].images.slice(0, 1)[0];
+      return product.variants[0].images.slice(0, 1)[0] as string;
     }
   } else {
     if (product?.images?.length > 0) {
-      return product.images.slice(0, 2); // Lấy 2 ảnh đầu tiên của sản phẩm chính
+      return product.images.slice(0, 2) as string[];
     }
 
     if (product?.variants?.length > 0) {
       const firstVariant = product.variants[0];
 
       if (firstVariant?.images?.length >= 2) {
-        return firstVariant.images.slice(0, 2); // Nếu variant đầu tiên có >= 2 ảnh
+        return firstVariant.images.slice(0, 2) as string[];
       }
 
       if (
         product.variants.length > 1 &&
         product.variants[1]?.images?.length > 0
       ) {
-        return [firstVariant.images[0], product.variants[1].images[0]]; // Lấy 1 ảnh từ mỗi variant
+        return [
+          firstVariant.images[0] as string,
+          product.variants[1].images[0] as string,
+        ];
       }
 
-      return firstVariant.images.slice(0, 1); // Chỉ có 1 ảnh duy nhất
+      return firstVariant.images.slice(0, 1) as string[];
     }
   }
 
