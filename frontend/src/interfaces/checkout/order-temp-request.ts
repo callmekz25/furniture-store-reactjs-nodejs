f9b9de8b057925586d0e62bb921e71d719fea491
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-06-29 21:55:11 +0700

    refactor

--- frontend/src/interfaces/checkout/order-temp-request.ts
+++
@@ -1,9 +0,0 @@
-import ICart from "../cart.interface";
-
-interface IOrderTempRequest {
-  products: ICart[];
-  note: string;
-  total_price: number;
-  total_items: number;
-}
-export default IOrderTempRequest;
