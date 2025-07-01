e70e77eb8f4de8d23cbf60e5ccbf52e49e814578
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-01 15:10:22 +0700

    layout

---
+++ frontend/src/layouts/main-layout.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
+import { Toaster } from "@/components/ui/sonner";
+import { Outlet } from "react-router-dom";
+import ChatBot from "@/components/chatbot/chat-bot";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <main className="bg-[#f7f7f7] min-h-screen">
+        <Outlet />
+        <ChatBot />
+      </main>
+      <Toaster />
+      <Footer />
+    </>
+  );
+};
+export default MainLayout;
