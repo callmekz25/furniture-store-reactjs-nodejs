e875f30738f950248c8e7f6b9c9a767f4fe6d46a
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-01 15:19:27 +0700

    generate variant to cart item

---
+++ frontend/src/utils/generate-cart-item.ts
@@ -0,0 +1,27 @@
+import IProduct from "@/interfaces/product/product.interface";
+import ICartItems from "@/interfaces/cart/cart-items.interface";
+import ISelectedVariant from "@/interfaces/product/selected-variant.interface";
+
+const generateCartItem = (product: IProduct): ICartItems => {
+  let availableVariant: ISelectedVariant | null | undefined = null;
+  if (product.variants && product.variants.length > 0) {
+    availableVariant = product.variants.find((v) => v.quantity > 0);
+  }
+
+  return {
+    productId: product._id!,
+    title: product.title,
+    quantity: 1,
+    image: availableVariant
+      ? (availableVariant.images[0] as string)
+      : (product.images[0] as string),
+    price: availableVariant ? availableVariant.price : product.price,
+    fakePrice: availableVariant
+      ? availableVariant.fakePrice
+      : product.fakePrice,
+    slug: product.slug,
+    discount: product.discount,
+    attributes: availableVariant ? availableVariant.attributes : null,
+  };
+};
+export default generateCartItem;
