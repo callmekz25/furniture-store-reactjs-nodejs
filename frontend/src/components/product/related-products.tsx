74778e117adf5924e3a1b3f2a8a3b5b31c2c4d22
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-01 15:05:49 +0700

    add related products

---
+++ frontend/src/components/product/related-products.tsx
@@ -0,0 +1,44 @@
+import CarouselProduct from "@/components/collections/carousel-products";
+import CardSkeleton from "@/components/loading/card-skeleton";
+import { useGetAll } from "@/hooks/useGet";
+import IProduct from "@/interfaces/product/product.interface";
+const RelatedProducts = ({ slug, title }: { title: string; slug: string }) => {
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useGetAll<IProduct[]>(
+    `/products/${slug}/related`,
+    ["related-products", slug],
+    false,
+    undefined,
+    {
+      limit: 8,
+    }
+  );
+  if (error) {
+    return <p>Lỗi xảy ra!</p>;
+  }
+  return (
+    <div className=" lg:px-3 pl-1.5 mt-10">
+      {isLoading ? (
+        <div className="flex items-center">
+          {[...Array(5)].map((_, i) => (
+            <CardSkeleton key={i} height={420} />
+          ))}
+        </div>
+      ) : products && products.length > 0 ? (
+        <CarouselProduct
+          products={products}
+          title={title}
+          more={false}
+          slug={slug}
+        />
+      ) : (
+        "Lỗi"
+      )}
+    </div>
+  );
+};
+
+export default RelatedProducts;
