bc825d01dccc30e2d827d8e0085314d4bbb853df
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-02 21:14:00 +0700

    fix button go to collection display based on total & limit

--- frontend/src/components/collections/collection-product.tsx
+++ frontend/src/components/collections/collection-product.tsx
@@ -1,21 +1,17 @@
 import CarouselProduct from "./carousel-products";
 import Loading from "../loading/loading";
 import { useGetAll } from "@/hooks/useGet";
-import IProduct from "@/interfaces/product/product.interface";
+import { Link } from "react-router-dom";
+import { ChevronRightIcon } from "lucide-react";
+import ICollectionLimitResponse from "@/interfaces/paginate-response/collection-limit-response";
 const CollectionProduct = ({
   slug,
   title,
-  more = false,
 }: {
   slug: string;
   title: string;
-  more: boolean;
 }) => {
-  const {
-    data: products,
-    isLoading,
-    error,
-  } = useGetAll<IProduct[]>(
+  const { data, isLoading, error } = useGetAll<ICollectionLimitResponse>(
     `/collections/${slug}/products`,
     ["products", slug],
     false,
@@ -33,13 +29,28 @@ const CollectionProduct = ({
     <div className="pb-[70px] lg:px-3 pl-1.5">
       {isLoading ? (
         <Loading />
-      ) : products && products.length > 0 ? (
-        <CarouselProduct
-          products={products}
-          title={title}
-          more={more}
-          slug={slug}
-        />
+      ) : data && data.products.length > 0 ? (
+        <div className="">
+          <CarouselProduct
+            products={data.products}
+            title={title}
+            total={data.total}
+            slug={slug}
+          />
+          <div
+            className={` items-center justify-center  mt-3 ${
+              data.total > 8 ? "flex" : "hidden"
+            }`}
+          >
+            <Link
+              to={`/collections/${slug}`}
+              style={{ boxShadow: "0 0 3px rgba(0, 0, 0, 0.08)" }}
+              className="flex justify-center  text-sm mt-4 transition-all duration-500 hover:bg-red-600 hover:text-white  items-center gap-1 font-medium bg-white  rounded py-3 px-1.5 min-w-[320px]"
+            >
+              Xem thêm <ChevronRightIcon className="size-5" />
+            </Link>
+          </div>
+        </div>
       ) : (
         "Lỗi"
       )}
