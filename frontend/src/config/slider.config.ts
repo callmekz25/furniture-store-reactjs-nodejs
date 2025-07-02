b49e91dc9bad1565723a518e294ec6c4c37e96c6
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-02 21:15:53 +0700

    config slider

---
+++ frontend/src/config/slider.config.ts
@@ -0,0 +1,152 @@
+export const settingGallery = {
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  arrows: false,
+  infinite: false,
+  vertical: true,
+  verticalSwiping: true,
+  swipeToSlide: true,
+  focusOnSelect: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        verticalSwiping: false,
+        vertical: false,
+      },
+    },
+  ],
+};
+
+export const settingHeroImages = {
+  dots: true,
+  speed: 800,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  infinite: true,
+  swipeToSlide: true,
+  initialSlide: 0,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  waitForAnimate: false,
+};
+export const settingCollection = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  initialSlide: 0,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2.1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 2.1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+export const settingBlogs = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  initialSlide: 0,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1.2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1.22,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+export const settingBathroom = {
+  dots: false,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  initialSlide: 0,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
