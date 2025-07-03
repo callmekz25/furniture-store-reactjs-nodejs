eac85b27dbbf044dca87b87713b6cc6efec57a1c
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-03 20:55:16 +0700

    add zoom image, auto slide image

--- frontend/src/components/product/product-gallery.tsx
+++ frontend/src/components/product/product-gallery.tsx
@@ -2,9 +2,15 @@ import { useRef, useState, useEffect, memo } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Lightbox from "yet-another-react-lightbox";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import type { SlideshowRef, ZoomRef } from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import ISelectedVariant from "@/interfaces/product/selected-variant.interface";
 import { settingGallery } from "@/config/slider.config";
+import ProgressLoading from "../loading/progress-loading";
 
 const ProductGallery = ({
   images,
@@ -13,6 +19,13 @@ const ProductGallery = ({
   images: string[];
   activeVariant?: ISelectedVariant | undefined | null;
 }) => {
+  const [openZoomImage, setOpenZoomImage] = useState(false);
+  const lightBoxSlideRef = useRef<SlideshowRef | null>(null);
+  const zoomRef = useRef<ZoomRef | null>(null);
+  const duration = 3000;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isZoomimg, setIsZooming] = useState(false);
+  const [progressKey, setProgressKey] = useState(1);
   const mainSliderRef = useRef<Slider | null>(null);
   const thumbSliderRef = useRef<Slider | null>(null);
   const [nav1, setNav1] = useState<Slider | null>(null);
@@ -62,6 +75,9 @@ const ProductGallery = ({
 
   return (
     <div className="w-full flex min-h-[400px] lg:flex-row flex-col-reverse lg:w-full lg:h-full h-auto  lg:max-h-[532px] max-h-none overflow-hidden">
+      {isPlaying && (
+        <ProgressLoading duration={duration} resetKey={progressKey} />
+      )}
       {/* Thumbnail Slider */}
       <div className="lg:w-[16%] w-full p-3 lg:h-auto min-h-[120px] flex-shrink-0">
         <Slider asNavFor={nav1!} {...thumbnailSettings} ref={thumbSliderRef}>
@@ -100,11 +116,53 @@ const ProductGallery = ({
                 src={img}
                 alt={img}
                 loading="lazy"
-                className="object-cover w-full max-w-full aspect-square "
+                onClick={() => setOpenZoomImage(true)}
+                className="object-cover hover:cursor-zoom-in w-full max-w-full aspect-square "
               />
             </div>
           ))}
         </Slider>
+        <Lightbox
+          className={`  ${
+            isZoomimg ? "hover:cursor-grab" : "hover:cursor-zoom-in"
+          }`}
+          plugins={[Slideshow, Zoom]}
+          zoom={{ ref: zoomRef }}
+          slideshow={{
+            ref: lightBoxSlideRef,
+            autoplay: false,
+            delay: duration,
+          }}
+          on={{
+            zoom: ({ zoom }: { zoom: number }) => {
+              if (zoom > 1) {
+                setIsZooming(true);
+              } else {
+                setIsZooming(false);
+              }
+            },
+            slideshowStart: () => setIsPlaying(true),
+            slideshowStop: () => setIsPlaying(false),
+            view: () => {
+              if (isPlaying) {
+                setProgressKey((prev) => prev + 1);
+              }
+            },
+          }}
+          carousel={{ finite: true }}
+          styles={{
+            container: { backgroundColor: "rgba(0, 0, 0, .8)" },
+          }}
+          render={{
+            buttonPrev: images.length <= 1 ? () => null : undefined,
+            buttonNext: images.length <= 1 ? () => null : undefined,
+          }}
+          open={openZoomImage}
+          close={() => setOpenZoomImage(false)}
+          slides={images.map((img) => {
+            return { src: img, alt: img };
+          })}
+        />
         <button
           className={`absolute right-0 z-10 top-1/2 -translate-y-1/2 ${
             currentIndex === images.length - 1 ? "hidden" : ""
