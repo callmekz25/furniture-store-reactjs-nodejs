a5f8973b4837080c40b6aec8f55fc11fef0ca5d1
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:46:50 +0700

    add get reviews by product id

--- frontend/src/services/review.service.ts
+++ frontend/src/services/review.service.ts
@@ -1,6 +1,16 @@
 import httpRequest from "../config/axios.config";
 
 import IReview from "@/interfaces/review.interface";
+
+export const getReviewsByProductId = async (id: string) => {
+  try {
+    const { data } = await httpRequest.get(`/products/${id}/reviews`);
+    return data;
+  } catch (error: any) {
+    throw new Error(error?.response?.data?.mess);
+  }
+};
+
 const postReview = async (request: IReview) => {
   try {
     const { productId } = request;
