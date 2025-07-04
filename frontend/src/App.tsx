e12353e073c2ef1674d0d81b16bf59cdd3b8ce55
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:37:42 +0700

    fix use custom hook to get user & cart

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,12 @@ import { ToastContainer } from "react-toastify";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Loading from "./components/loading/loading";
-import { useGetOne } from "./hooks/useGet";
+import { useGetUser } from "./hooks/use-account";
+import { useGetCart } from "./hooks/use-cart";
 
 const App = () => {
-  const { isLoading } = useGetOne("/get-user", ["user"], true);
-  const { isLoading: isCartLoading } = useGetOne("/cart", ["cart"], true);
+  const { isLoading } = useGetUser();
+  const { isLoading: isCartLoading } = useGetCart();
 
   if (isLoading || isCartLoading) {
     return <Loading />;
