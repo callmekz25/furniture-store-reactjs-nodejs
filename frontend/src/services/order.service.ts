d1ddea8384caf3c4971cabc10264785a837aedca
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:46:25 +0700

    add get order by id

--- frontend/src/services/order.service.ts
+++ frontend/src/services/order.service.ts
@@ -1,6 +1,15 @@
 import httpRequest from "../config/axios.config";
 import IOrderTempRequest from "@/interfaces/order/order-temp-request";
 
+export const getOrderById = async (id: string) => {
+  try {
+    const { data } = await httpRequest.get(`/checkouts/${id}`);
+    return data;
+  } catch (error) {
+    throw new Error(error?.response?.data?.message);
+  }
+};
+
 export const createOrderDraft = async (request: IOrderTempRequest) => {
   try {
     const { data } = await httpRequest.post("/checkouts", request);
