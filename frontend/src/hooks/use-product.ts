8b226a838958e70b05f013e5827ae6e4b76a98ca
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:45:08 +0700

    hook to get, add, update products

---
+++ frontend/src/hooks/use-product.ts
@@ -0,0 +1,99 @@
+import ICollectionLimitResponse from "@/interfaces/paginate-response/collection-limit-response";
+import CollectionResponse from "@/interfaces/paginate-response/collection-response";
+import IProduct from "@/interfaces/product/product.interface";
+import {
+  getProductById,
+  getProductBySlug,
+  getProducts,
+  getProductsByCollection,
+  getProductsBySearch,
+  updateProduct,
+} from "@/services/product.service";
+import searchParamsToObject from "@/utils/search-params-to-object";
+import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
+
+export const useGetProducts = () => {
+  return useQuery<IProduct[]>({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
+};
+
+export const useGetProductById = (id: string) => {
+  return useQuery<IProduct>({
+    queryKey: ["products", id],
+    queryFn: () => getProductById(id),
+    enabled: !!id,
+  });
+};
+export const useGetProductBySlug = (slug: string) => {
+  return useQuery<IProduct>({
+    queryKey: ["products", slug],
+    queryFn: () => getProductBySlug(slug),
+    enabled: !!slug,
+  });
+};
+export const useGetProductsBySearch = (query: string) => {
+  return useQuery<ICollectionLimitResponse>({
+    queryKey: ["search", query],
+    queryFn: () => getProductsBySearch({ q: query }),
+    enabled: !!query,
+  });
+};
+
+export const useGetInfiniteProductsBySearch = (query: string) => {
+  return useInfiniteQuery<CollectionResponse>({
+    queryKey: ["search", query, "all"],
+    queryFn: ({ pageParam = 1 }) =>
+      getProductsBySearch({
+        q: query,
+        page: pageParam,
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const limit = 20;
+      return allPages.length * limit < lastPage.total
+        ? allPages.length + 1
+        : undefined;
+    },
+  });
+};
+export const useGetInfiniteProductsByCollection = (
+  collection: string,
+  query: URLSearchParams
+) => {
+  const paramsToObject = searchParamsToObject(query);
+  return useInfiniteQuery<CollectionResponse>({
+    queryKey: ["collections", collection, query.toString()],
+    queryFn: ({ pageParam = 1 }) =>
+      getProductsByCollection(collection, {
+        ...paramsToObject,
+        page: pageParam,
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      const limit = 20;
+      return allPages.length * limit < lastPage.total
+        ? allPages.length + 1
+        : undefined;
+    },
+  });
+};
+export const useGetProductsByCollection = (
+  collection: string,
+  limit?: number
+) => {
+  const query = limit && limit > 0 ? { limit } : undefined;
+  return useQuery<ICollectionLimitResponse>({
+    queryKey: ["collections", collection],
+    queryFn: () => getProductsByCollection(collection, query, true),
+    enabled: !!collection,
+  });
+};
+
+export const useUpdateProduct = () => {
+  return useMutation({
+    mutationFn: ({ id, collections }: { id: string; collections: string[] }) =>
+      updateProduct(id, collections),
+  });
+};
