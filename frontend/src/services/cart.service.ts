626c1dbb23cedac4090279eadf35e99523a07754
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:46:09 +0700

    add get cart

--- frontend/src/services/cart.service.ts
+++ frontend/src/services/cart.service.ts
@@ -1,6 +1,16 @@
 import IUpdateCartRequest from "@/interfaces/cart/update-cart-request.interface";
 import httpRequest from "../config/axios.config";
 import ICartItems from "@/interfaces/cart/cart-items.interface";
+
+export const getCart = async () => {
+  try {
+    const { data } = await httpRequest.get("/cart");
+    return data;
+  } catch (error: any) {
+    throw new Error(error?.response?.data?.message);
+  }
+};
+
 const addCart = async (cart: ICartItems) => {
   try {
     const { data } = await httpRequest.post("/cart", cart);
