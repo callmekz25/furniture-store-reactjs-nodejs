34d6f80d5adc9908b11892dfd30f97d679dcee49
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:42:04 +0700

    rename

--- frontend/src/hooks/chat.ts
+++
@@ -1,8 +0,0 @@
-import { sendChatMessage } from "@/services/chat.service";
-import { useMutation } from "@tanstack/react-query";
-
-export const useSendMessage = () => {
-  return useMutation({
-    mutationFn: (message: string) => sendChatMessage(message),
-  });
-};
--- frontend/src/hooks/payment.ts
+++
@@ -1,9 +0,0 @@
-import ICheckoutRequest from "@/interfaces/checkout/payment-request";
-import { createPayment } from "@/services/payment.service";
-import { useMutation } from "@tanstack/react-query";
-
-export const usePayment = () => {
-  return useMutation({
-    mutationFn: (request: ICheckoutRequest) => createPayment(request),
-  });
-};
