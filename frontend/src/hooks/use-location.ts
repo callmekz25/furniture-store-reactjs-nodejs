3185169c112dab51d6d1d591e317cc4850d5ef27
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-04 18:44:31 +0700

    hook to get location

---
+++ frontend/src/hooks/use-location.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  getProvinces,
+  getDistrictsByProvinceId,
+  getWardsByDistrictId,
+} from "@/services/location.service";
+export const useGetProvinces = () => {
+  return useQuery({
+    queryKey: ["provinces"],
+    queryFn: getProvinces,
+  });
+};
+export const useGetDistricts = (provinceId: string) => {
+  return useQuery({
+    queryKey: ["districts", provinceId],
+    queryFn: () => getDistrictsByProvinceId(provinceId),
+    enabled: !!provinceId,
+  });
+};
+export const useGetWards = (districtId: string) => {
+  return useQuery({
+    queryKey: ["districts", districtId],
+    queryFn: () => getWardsByDistrictId(districtId),
+    enabled: !!districtId,
+  });
+};
