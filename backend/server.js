7da5ea2d949df439a0ee6fa0acd206dbd696f158
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-06 14:22:16 +0700

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://furniture-store-reactjs-nodejs-e51z.vercel.app",
+      "https://furniture-store-reactjs-nodejs.vercel.app",
     ],
     credentials: true,
   })
