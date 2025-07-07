93b7941f935cad360e354d78857d06174dfa175f
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-07 19:59:47 +0700

    add function calling to read data & response base on data

---
+++ backend/functions-calling/product.fn-declaration.js
@@ -0,0 +1,10 @@
+import { Type } from "@google/genai";
+export const getProductsDeclaration = {
+  name: "get_products",
+  description: "Gets all products in the store",
+  parameters: {
+    type: Type.OBJECT,
+    properties: {},
+    required: [],
+  },
+};
