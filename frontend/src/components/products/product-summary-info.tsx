ffef13298e6228c8a2da303ee75a19e73a6acfd4
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-12 21:40:47 +0700

    separate to a component to easy to handle & debug

---
+++ frontend/src/components/products/product-summary-info.tsx
@@ -0,0 +1,74 @@
+import IProduct from "@/interfaces/product/product.interface";
+import ISelectedVariant from "@/interfaces/product/selected-variant.interface";
+import checkInStock from "@/utils/check-instock";
+import formatPriceToVND from "@/utils/format-price";
+import getFinalPrice from "@/utils/get-final-price";
+import getPrice from "@/utils/get-price";
+
+const ProductSummaryInfo = ({
+  product,
+  selectedVariant,
+}: {
+  product: IProduct;
+  selectedVariant: ISelectedVariant;
+}) => {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold text-[#c31425]">{product!.title}</h3>
+      <div className="flex items-center gap-3 mt-2 flex-wrap  text-sm font-normal">
+        <div className="flex items-center gap-1">
+          <span>Mã sản phẩm:</span>
+          {product.variants?.length > 0 && selectedVariant ? (
+            <span className="font-bold text-[#c31425]">
+              {selectedVariant.sku}
+            </span>
+          ) : (
+            product?.sku && (
+              <span className="font-bold text-[#c31425]">{product.sku}</span>
+            )
+          )}
+        </div>
+        <span>|</span>
+        <div className="flex items-center gap-1">
+          <span>Tình trạng:</span>
+          <span className="font-bold text-[#c31425]">
+            {checkInStock(product, selectedVariant) ? "Còn hàng" : "Hết hàng"}
+          </span>
+        </div>
+        <span>|</span>
+        <div className="flex items-center gap-1">
+          <span>Thương hiệu:</span>
+          <span className="font-bold text-[#c31425] uppercase">
+            {product!.brand}
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center py-4 mt-4 bg-[#fafafa] px-4">
+        <span className="text-sm font-semibold lg:block hidden min-w-[100px]">
+          Giá:
+        </span>
+        <div className="">
+          {product.promotion ? (
+            <div className="flex items-center gap-4">
+              <span className="font-bold lg:text-[28px]  text-[22px] text-red-500">
+                {formatPriceToVND(getFinalPrice(product, selectedVariant))}
+              </span>
+              <span className="lg:text-[18px] line-through opacity-50 font-normal">
+                {formatPriceToVND(getPrice(product, selectedVariant))}
+              </span>
+              <div className="border border-red-500 bg-white py-[3px] lg:px-[14px] px-2.5 lg:text-[13px] text-[10px] rounded-[3px] text-red-500 font-bold">
+                -{product.promotion.discountValue}%
+              </div>
+            </div>
+          ) : (
+            <span className="font-bold lg:text-3xl  text-[22px] text-red-500">
+              {formatPriceToVND(getPrice(product, selectedVariant))}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSummaryInfo;
--- frontend/src/pages/product/product-detail.tsx
+++ frontend/src/pages/product/product-detail.tsx
@@ -1,40 +1,22 @@
-import { PlusIcon, MinusIcon } from "@heroicons/react/24/outline";
-import Guard from "../../assets/guard.webp";
-import Refund from "../../assets/refund.webp";
-import Hotline from "../../assets/hotline.webp";
 import { memo, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import formatPriceToVND from "@/utils/format-price";
+import { useParams } from "react-router-dom";
 import ProductGallery from "@/components/gallery/product-gallery";
-import { useAppSelector } from "@/redux/hook";
 import { addRecentlyViewedProduct } from "@/services/product.service";
 import RecentlyViewProductsList from "@/components/sections/showcase/recently-view";
-import ProductRating from "@/components/rates/product-rating";
 import RelatedProducts from "@/components/sections/showcase/related-products";
 import Loading from "@/components/loading/loading";
-import { CustomToastify, ToastifyError } from "@/helpers/custom-toastify";
 import ProductVariants from "@/components/variants/product-variants";
 import Error from "../shared/error";
-import { useAddToCart } from "@/hooks/use-cart";
-import { useQueryClient } from "@tanstack/react-query";
 import ISelectedVariant from "@/interfaces/product/selected-variant.interface";
-import ICartItems from "@/interfaces/cart/cart-items.interface";
 import { useGetProductBySlug } from "@/hooks/use-product";
-import checkInstock from "@/utils/check-instock";
-import getPrice from "@/utils/get-price";
-import checkInStock from "@/utils/check-instock";
-import getFinalPrice from "@/utils/get-final-price";
+import PolicySection from "@/components/sections/policy/policy-section";
+import AddToCartActions from "@/components/products/add-to-cart-actions";
+import ProductSummaryInfo from "@/components/products/product-summary-info";
+import ProductTabs from "@/components/products/product-tabs";
 
 const ProductDetail = () => {
-  const [isExpand, setIsExpand] = useState<boolean>(false);
-  const [isTabDescr, setIsTabDescr] = useState<boolean>(true);
-  const [quantity, setQuantity] = useState<number>(1);
   const [selectedVariant, setSelectedVariant] =
     useState<ISelectedVariant | null>(null);
-  const navigate = useNavigate();
-  const { isOpen } = useAppSelector((state) => state.cart);
-  const queryClient = useQueryClient();
-  const { mutate: addToCart, isPending } = useAddToCart();
   const { slug } = useParams<string>();
   const { data: product, isLoading, error } = useGetProductBySlug(slug!);
 
@@ -43,55 +25,10 @@ const ProductDetail = () => {
     addRecentlyViewedProduct(product);
   }, [product]);
 
-  // Submit add cart
-  const handleAddCart = async (redirect: boolean) => {
-    if (!product) return;
-
-    const data: ICartItems = {
-      productId: product._id!,
-      title: product.title,
-      quantity,
-      image: selectedVariant
-        ? (selectedVariant.images[0] as string)
-        : (product.images[0] as string),
-      price: selectedVariant ? selectedVariant.price : product.price,
-      slug: product.slug,
-      attributes: selectedVariant ? selectedVariant.attributes : null,
-    };
-
-    addToCart(data, {
-      onSuccess: (data) => {
-        if (redirect) navigate("/cart");
-        else
-          CustomToastify({
-            image: selectedVariant
-              ? (selectedVariant.images[0] as string)
-              : (product.images[0] as string),
-            price: selectedVariant ? selectedVariant.price : product.price,
-            title: product.title,
-          });
-        queryClient.setQueryData(["cart"], data);
-      },
-      onError: (error) => ToastifyError(error.message),
-    });
-  };
-
-  const plusQuantity = () => {
-    setQuantity((prev) => prev + 1);
-  };
-  const minusQuantity = () => {
-    if (quantity <= 1) {
-      return;
-    } else {
-      setQuantity((prev) => prev - 1);
-    }
-  };
   // Group images variants to 1 array
   const allImages =
     product?.variants?.flatMap((variant: ISelectedVariant) => variant.images) ??
     [];
-
-  console.log(product);
 
   if (error) {
     return <Error />;
@@ -99,7 +36,6 @@ const ProductDetail = () => {
 
   return (
     <div className="pt-6 pb-10 break-point ">
-      {isPending && <Loading />}
       {isLoading ? (
         <Loading />
       ) : (
@@ -109,7 +45,7 @@ const ProductDetail = () => {
               {product &&
                 (product.images && product.images.length > 0 ? (
                   <ProductGallery images={product.images} />
-                ) : product.variants && product.variants.length > 0 ? (
+                ) : product?.variants?.length > 0 ? (
                   <ProductGallery
                     images={allImages as string[]}
                     activeVariant={selectedVariant}
@@ -125,277 +61,23 @@ const ProductDetail = () => {
             </div>
             <div className=" lg:w-[55%]">
               <div className="bg-white px-4  py-5">
-                <h3 className="text-2xl font-bold text-[#c31425]">
-                  {product!.title}
-                </h3>
-                <div className="flex items-center gap-3 mt-2 flex-wrap  text-sm font-normal">
-                  <div className="flex items-center gap-1">
-                    <span>Mã sản phẩm:</span>
-                    {product.variants?.length > 0 && selectedVariant ? (
-                      <span className="font-bold text-[#c31425]">
-                        {selectedVariant.sku}
-                      </span>
-                    ) : (
-                      product?.sku && (
-                        <span className="font-bold text-[#c31425]">
-                          {product.sku}
-                        </span>
-                      )
-                    )}
-                  </div>
-                  <span>|</span>
-                  <div className="flex items-center gap-1">
-                    <span>Tình trạng:</span>
-                    <span className="font-bold text-[#c31425]">
-                      {checkInStock(product, selectedVariant)
-                        ? "Còn hàng"
-                        : "Hết hàng"}
-                    </span>
-                  </div>
-                  <span>|</span>
-                  <div className="flex items-center gap-1">
-                    <span>Thương hiệu:</span>
-                    <span className="font-bold text-[#c31425] uppercase">
-                      {product!.brand}
-                    </span>
-                  </div>
-                </div>
-                <div className="flex items-center py-4 mt-4 bg-[#fafafa] px-4">
-                  <span className="text-sm font-semibold lg:block hidden min-w-[100px]">
-                    Giá:
-                  </span>
-                  <div className="">
-                    {product.promotion ? (
-                      <div className="flex items-center gap-4">
-                        <span className="font-bold lg:text-[28px]  text-[22px] text-red-500">
-                          {formatPriceToVND(
-                            getFinalPrice(product, selectedVariant)
-                          )}
-                        </span>
-                        <span className="lg:text-[18px] line-through opacity-50 font-normal">
-                          {formatPriceToVND(getPrice(product, selectedVariant))}
-                        </span>
-                        <div className="border border-red-500 bg-white py-[3px] lg:px-[14px] px-2.5 lg:text-[13px] text-[10px] rounded-[3px] text-red-500 font-bold">
-                          -{product.promotion.discountValue}%
-                        </div>
-                      </div>
-                    ) : (
-                      <span className="font-bold lg:text-3xl  text-[22px] text-red-500">
-                        {formatPriceToVND(getPrice(product, selectedVariant))}
-                      </span>
-                    )}
-                  </div>
-                </div>
-                {product && (
-                  <ProductVariants
-                    variants={product.variants ?? []}
-                    onSelectVariant={setSelectedVariant}
-                  />
-                )}
+                <ProductSummaryInfo
+                  product={product}
+                  selectedVariant={selectedVariant}
+                />
+                <ProductVariants
+                  variants={product.variants ?? []}
+                  onSelectVariant={setSelectedVariant}
+                />
 
-                <div className=" items-center  px-4 mt-6 lg:flex hidden">
-                  <span className="text-sm font-semibold min-w-[100px]">
-                    Số lượng:
-                  </span>
-                  <div className="flex items-center ">
-                    <button
-                      onClick={() => minusQuantity()}
-                      className="border border-gray-100 p-2  bg-[#f9f9f9]"
-                    >
-                      <MinusIcon className="size-4" />
-                    </button>
-                    <span className="border font-semibold border-gray-100 px-4 py-1.5 text-sm ">
-                      {quantity}
-                    </span>
-                    <button
-                      onClick={() => plusQuantity()}
-                      className="border border-gray-100 p-2  bg-[#f9f9f9]"
-                    >
-                      <PlusIcon className="size-4" />
-                    </button>
-                  </div>
-                </div>
-                <div className="lg:flex items-center hidden gap-4 mt-6 font-medium">
-                  {checkInstock(product) ? (
-                    <>
-                      <button
-                        onClick={() => handleAddCart(false)}
-                        disabled={!checkInstock(product)}
-                        className={`border transition-all duration-500 hover:opacity-80 border-red-500 rounded px-4 py-2.5  items-center justify-center uppercase font-bold w-full text-red-500 ${
-                          checkInstock(product) ? "flex" : "hidden"
-                        }`}
-                      >
-                        Thêm vào giỏ
-                      </button>
-                      <button
-                        onClick={() => handleAddCart(true)}
-                        disabled={!checkInstock(product)}
-                        className={`border transition-all duration-500 hover:opacity-80 font-bold  bg-[#ff0000] text-white rounded px-4 py-[11px] uppercase flex items-center justify-center w-full ${
-                          checkInstock(product) ? "flex" : "hidden"
-                        }`}
-                      >
-                        Mua ngay
-                      </button>
-                    </>
-                  ) : (
-                    <button
-                      disabled
-                      className="border border-gray-500  rounded-sm uppercase px-3 text-[#929292] text-[15px] font-bold py-2.5 w-full "
-                    >
-                      Hết hàng
-                    </button>
-                  )}
-                </div>
-                {/* Mobile add cart */}
-                <div
-                  className={`flex lg:hidden items-center z-50  left-0  py-2 border-t border-gray-200 bg-white w-full font-semibold fixed bottom-0 ${
-                    isOpen ? "hidden" : "flex"
-                  }`}
-                >
-                  <div className="break-point px-4 flex items-center gap-2  w-full">
-                    <div className="flex w-[140px] items-center  ">
-                      <button className="border border-gray-100 flex items-center justify-center p-1.5 size-10 bg-[#f3f4f4] ">
-                        <MinusIcon className="size-4 " />
-                      </button>
-                      <p className="border h-10 w-12 flex items-center justify-center font-semibold border-gray-100 px-4 py-1 text-sm ">
-                        {quantity}
-                      </p>
-                      <button className="border border-gray-100 flex items-center justify-center p-1.5 size-10  bg-[#f3f4f4]">
-                        <PlusIcon className="size-4" />
-                      </button>
-                    </div>
-                    {checkInstock(product) ? (
-                      <button
-                        onClick={() => handleAddCart(false)}
-                        style={{ width: "calc(100% - 140px)" }}
-                        className="border  transition-all duration-500 hover:opacity-80 font-medium text-sm  bg-[#ff0000] text-white rounded px-4 py-2.5 uppercase flex items-center justify-center w-full"
-                      >
-                        Thêm vào giỏ
-                      </button>
-                    ) : (
-                      <button
-                        disabled
-                        className="border border-gray-500  rounded uppercase px-[15px] text-[#929292] text-[13px] font-medium h-[40px] w-full "
-                      >
-                        Hết hàng
-                      </button>
-                    )}
-                  </div>
-                </div>
-                <div className="flex lg:flex-row flex-col lg:items-center items-start gap-2 lg:gap-0 justify-between mt-6">
-                  <div className="flex items-center gap-2 flex-1">
-                    <img
-                      src={Guard}
-                      alt="Bảo hành"
-                      loading="lazy"
-                      className="size-7  object-contain"
-                    />
-                    <span className="text-sm">1 Năm Bảo Hành</span>
-                  </div>
-                  <div className="flex items-center gap-2 flex-1">
-                    <img
-                      src={Refund}
-                      alt="Bảo hành"
-                      loading="lazy"
-                      className="size-7  object-contain"
-                    />
-                    <span className="text-sm">
-                      Hỗ trợ đổi trong 3 ngày cho sản phẩm nguyên giá
-                    </span>
-                  </div>
-                  <div className="flex items-center justify-end gap-2 flex-1">
-                    <img
-                      src={Hotline}
-                      alt="Bảo hành"
-                      loading="lazy"
-                      className="size-7 object-contain"
-                    />
-                    <span className="text-sm">
-                      Hotline: <span className="font-bold">0899348258</span>
-                    </span>
-                  </div>
-                </div>
+                <AddToCartActions
+                  product={product}
+                  selectedVariant={selectedVariant}
+                />
+                <PolicySection />
               </div>
 
-              <div className="mt-4 bg-white px-4 py-2 ">
-                <ul className="flex items-center gap-10 border-b border-gray-300 py-3">
-                  <li
-                    className={`font-semibold text-md ${
-                      isTabDescr ? "color-red " : ""
-                    }`}
-                  >
-                    <button
-                      onClick={() => {
-                        setIsExpand(false);
-                        setIsTabDescr(true);
-                      }}
-                    >
-                      Mô tả sản phẩm
-                    </button>
-                  </li>
-                  <li
-                    className={`font-semibold text-md ${
-                      isTabDescr ? " " : "color-red"
-                    }`}
-                  >
-                    <button
-                      onClick={() => {
-                        setIsExpand(false);
-                        setIsTabDescr(false);
-                      }}
-                    >
-                      Đánh giá
-                    </button>
-                  </li>
-                </ul>
-                <div
-                  className={`overflow-hidden min-w-full py-4 text-sm transition-all duration-500 product-description  ease-out relative ${
-                    isExpand ? "max-h-[1000px]" : "max-h-[230px]"
-                  } `}
-                >
-                  {isTabDescr ? (
-                    <>
-                      {product?.descr ? (
-                        <div
-                          className="whitespace-pre-wrap text-sm "
-                          dangerouslySetInnerHTML={{ __html: product?.descr }}
-                        />
-                      ) : (
-                        <span className="">Chưa có mô tả cho sản phẩm này</span>
-                      )}
-                    </>
-                  ) : (
-                    product &&
-                    product?._id && <ProductRating productId={product._id} />
-                  )}
-
-                  <div
-                    className={`absolute bottom-0 transition-all duration-300 left-0 w-full h-20 bg-gradient-to-t from-white via-white/80 to-transparent ${
-                      isExpand ? "opacity-0 hidden" : "opacity-100 block"
-                    } ${isTabDescr && !product?.descr ? "hidden" : ""} `}
-                  />
-                </div>
-                <div className="flex items-center justify-center">
-                  <button
-                    className={`flex transition-all duration-300 hover:bg-red-700 hover:text-white items-center color-red gap-2 border rounded border-red-600 px-3 py-1.5 text-sm ${
-                      isTabDescr && !product?.descr ? "hidden" : ""
-                    } `}
-                    onClick={() => setIsExpand((prev) => !prev)}
-                  >
-                    {isExpand ? (
-                      <>
-                        <MinusIcon className="size-4" />
-                        Rút gọn
-                      </>
-                    ) : (
-                      <>
-                        <PlusIcon className="size-4" />
-                        Hiển thị thêm
-                      </>
-                    )}
-                  </button>
-                </div>
-              </div>
+              <ProductTabs product={product} />
             </div>
           </section>
           {product && product._id && (
