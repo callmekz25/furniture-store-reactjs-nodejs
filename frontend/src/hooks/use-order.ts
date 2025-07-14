a50f75df9504b8d6123c225c9c1a999644129f7a
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-07-14 23:28:37 +0700

    rename place order

--- frontend/src/hooks/use-order.ts
+++ frontend/src/hooks/use-order.ts
@@ -1,6 +1,6 @@
 import IOrderTempRequest from "@/interfaces/order/order-temp-request";
 import IOrder from "@/interfaces/order/order.interface";
-import { createOrderDraft, getOrderById } from "@/services/order.service";
+import { placeTempOrder, getOrderById } from "@/services/order.service";
 import { useMutation, useQuery } from "@tanstack/react-query";
 export const useGetOrderById = (id: string) => {
   return useQuery<IOrder>({
@@ -11,6 +11,6 @@ export const useGetOrderById = (id: string) => {
 };
 export const useCreateOrderTemp = () => {
   return useMutation({
-    mutationFn: (request: IOrderTempRequest) => createOrderDraft(request),
+    mutationFn: (request: IOrderTempRequest) => placeTempOrder(request),
   });
 };
