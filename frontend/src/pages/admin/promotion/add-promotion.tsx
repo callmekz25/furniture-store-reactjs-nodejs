979e427785bd555face998fcdeffa7b37511fb84
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-10-02 21:45:03 +0700

    add type

--- frontend/src/pages/admin/promotion/add-promotion.tsx
+++ frontend/src/pages/admin/promotion/add-promotion.tsx
@@ -1,19 +1,20 @@
-import { useForm } from "react-hook-form";
-import IPromotion from "@/interfaces/promotion/promotion.interface";
-import { useAddPromotion } from "@/hooks/use-promotion";
-import { toast } from "sonner";
-import PromotionForm from "@/components/admin/forms/promotion-form";
+import { useForm } from 'react-hook-form';
+import IPromotion from '@/interfaces/promotion/promotion.interface';
+import { useAddPromotion } from '@/hooks/use-promotion';
+import { toast } from 'sonner';
+import PromotionForm from '@/components/admin/forms/promotion-form';
+import { IOptionMultiSelect } from '@/interfaces/multi-select/option.interface';
 const AddPromotion = () => {
   const form = useForm<IPromotion>({
     defaultValues: {
-      name: "",
-      descr: "",
+      name: '',
+      descr: '',
       scope: {
-        type: "all",
+        type: 'all',
         ids: [],
       },
-      startDate: "",
-      endDate: "",
+      startDate: '',
+      endDate: '',
       discountValue: 0,
       isActive: false,
     },
@@ -23,7 +24,7 @@ const AddPromotion = () => {
   const { mutate: addPromotion, isPending } = useAddPromotion();
 
   const handleAddPromotion = (data: IPromotion) => {
-    const ids = data.scope.ids.map((d) => d._id);
+    const ids = data.scope.ids.map((d) => (d as IOptionMultiSelect)._id);
     const payload: IPromotion = {
       ...data,
       scope: {
@@ -33,14 +34,14 @@ const AddPromotion = () => {
     };
     addPromotion(payload, {
       onSuccess: () => {
-        toast.success("Thêm mới thành công", {
-          position: "top-right",
+        toast.success('Thêm mới thành công', {
+          position: 'top-right',
         });
         reset();
       },
       onError: () =>
-        toast.error("Oops xảy ra lỗi", {
-          position: "top-right",
+        toast.error('Oops xảy ra lỗi', {
+          position: 'top-right',
         }),
     });
   };
